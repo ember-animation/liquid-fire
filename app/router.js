e7fd4a20f72e2362b8c8adca0cd640fccce42bcd
Edward Faulkner <ef@alum.mit.edu>
2014-07-04 14:34:48 -0400

    new repo

---
+++ app/router.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+var Router = Ember.Router.extend({
+  location: HtmlbarsDemoENV.locationType
+});
+
+Router.map(function() {
+});
+
+export default Router;
---
+++ tests/helpers/resolver.js
@@ -0,0 +1,9 @@
+import Resolver from 'ember/resolver';
+
+var resolver = Resolver.create();
+
+resolver.namespace = {
+  modulePrefix: 'htmlbars-demo'
+};
+
+export default resolver;
