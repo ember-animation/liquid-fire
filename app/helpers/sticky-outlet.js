0fd2a5f4f903489c1fab283fed8b7a84d01c830c
Edward Faulkner <ef@alum.mit.edu>
2014-07-04 17:03:38 -0400

    custom helper

---
+++ app/helpers/sticky-outlet.js
@@ -0,0 +1,8 @@
+export default function stickyOutletHelper(property, options) {
+  if (property && property.data && property.data.isRenderData) {
+    options = property;
+    property = 'main';
+  }
+  options.hash.view = 'sticky-outlet';
+  return Ember.Handlebars.helpers.outlet.call(this, property, options);
+}
