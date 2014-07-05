531f1876bedd17ffc79fa6d7fa8f33b6091f35f6
Edward Faulkner <ef@alum.mit.edu>
2014-07-05 01:47:32 -0400

    switching to velocity.js. support both parallel and serial

--- README.md
+++ README.md
@@ -1,25 +1,57 @@
-# Htmlbars-demo
+API design todos:
+- serial vs parallel transition
+- pluggable "done" handling. Promise-based.
+- directionality
 
-This README outlines the details of collaborating on this Ember application.
+Thoughts
+--------
 
-## Installation
+### Architectural
 
-* `git clone` this repository
-* `npm install`
-* `bower install`
+Cross-route animated transitions don't fit neatly into any single part
+of the Ember architecture. You want the view layer to deal with
+actually moving DIVs around, etc. But you want the routing layer to
+decide what the spatial relationship between routes should be.
 
-## Running
+We split these concerns by establishing a directionality API between
+routes and views. Routes just say "I'm to the left (or right, or
+above, etc) of that other route". Views need to know how to transition
+appropriate in the direction they're told.
 
-* `ember server`
-* Visit your app at http://localhost:4200.
+### Spatial Descriptors
 
-## Running Tests
+A transition from route A with params Pa to route B with params Pb can
+have one of the following spatial relationships:
 
-* `ember test`
-* `ember test --server`
+- none (there's no spatial relationship. Views can interpret this how
+  they want -- maybe no transition, maybe a default transition.)
+- left
+- right
+- up
+- down
+- front
+- back
 
-## Building
+The relationship may depend on the parameters of the routes.
 
-* `ember build`
+They are not necessarily reflexive or transitive (we're not dealing
+with real space). But in a sane app they often will be.
 
-For more information on using ember-cli, visit [http://iamstef.net/ember-cli/](http://iamstef.net/ember-cli/).
+### Types of Animations
+
+- within a single route, scalar context (done already in previous animation-demo)
+- within a single route, model context (can be readily extended from the preceeding)
+- between traditional routes (the new sticky-outlet + route directionality api)
+- between morphing routes (ooh shiny)
+
+
+### Morphing Routes
+
+The goal is to do something like the Contacts example here:
+
+http://www.google.com/design/spec/animation/meaningful-transitions.html
+
+I think we can do it by building
+ - a single dual-purple view & controller that have morphing capabilities
+- a (child) route that potentially loads new data as usual and adds it
+  to the controller, but doesn't touch outlets at all by itself.
--- app/views/sticky-outlet.js
+++ app/views/sticky-outlet.js
@@ -1,36 +1,43 @@
 var StickyChild = Ember.ContainerView.extend({
   classNames: ['sticky-child'],
-  classNameBindings: ['entering', 'leaving'],
   entering: true,
   
-  leaving: Ember.computed(function(){
-    var parentCurrentView = this.get('parentView.currentView');
-    return this !== parentCurrentView && this.get('currentView') !== parentCurrentView;
-  }).property('parentView.currentView'),
+  animateIn: function(){
+    var duration = this.get('entering') ? 250 : 0,
+	self = this;
+    return new Ember.RSVP.Promise(function(resolve){
+      console.log("starting animateIn for", self.get('currentView.renderedName'));
+      self.$().velocity({translateX: ["0%", "100%"]}, {duration: duration, complete: resolve})
+    }).then(function(){console.log("finished animateIn for", self.get('currentView.renderedName'))});
+  }.on('didInsertElement'),
 
-  didInsertElement: function(){
+  animateOut: function(){
+    if (this._leaving) {
+      return this._leaving;
+    }
     var self = this;
-    this.$().on('transitionEnd webkitTransitionEnd', function(){
-      if (self.get('leaving')) {
-	self.destroy();
-      }
-    });
-    Ember.run.next(function(){
-      self.set('entering', false);
-    });
+    return this._leaving = new Ember.RSVP.Promise(function(resolve) {
+      console.log("starting animateOut for", self.get('currentView.renderedName'));
+      self.$().velocity({translateX: '-100%'}, {duration: 250, complete: resolve})
+    }).then(function(){ console.log("finished animateOut for ", self.get('currentView.renderedName')); self.destroy() });
   }
+  
 });
 
 export default Ember.ContainerView.extend({
   classNames: ['sticky-outlet'],
 
-
+  // Should the old view leaving and the new view entering run in
+  // parallel? Otherwise, they run in series.
+  parallel: true,
+  
   init: function(){
     var currentView = this.get('currentView');
     if (currentView) {
       this.set('currentView', StickyChild.create({currentView: currentView, entering: false}));
     }
-   this._super();
+    this.set('inboundQueue', []);
+    this._super();
   },
   
   // Deliberately overriding a private method from
@@ -42,14 +49,28 @@ export default Ember.ContainerView.extend({
 
   // Deliberately overriding a private method from
   // Ember.ContainerView!
-  //
-  // We need to wrap our new child in our helper view.
   _currentViewDidChange: Ember.observer('currentView', function() {
+    var promises = this.get('childViews').map(function(child) {
+      return child.animateOut();
+    });
+
+    if (this.get('parallel')) {
+      this._pushCurrent();
+    } else {
+      var self = this;
+      Ember.RSVP.allSettled(promises).then(function(){
+	self._pushCurrent();
+      });
+    }
+
+  }),
+  
+  _pushCurrent: function() {
     var currentView = this.get('currentView');
     if (currentView) {
       this.pushObject(StickyChild.create({currentView: currentView}));
     }
-  })
+  }
 
 
 });
