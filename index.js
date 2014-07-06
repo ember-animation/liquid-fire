f6673faa68ef0a2687cd0406bce4e01791c82b62
Edward Faulkner <ef@alum.mit.edu>
2014-07-05 22:21:54 -0400

    working addon

--- index.js
+++ index.js
@@ -18,17 +18,17 @@ function unwatchedTree(dir) {
 }
 
 EmberAnimate.prototype.treeFor = function treeFor(name) {
-  var treePath =  path.join('node_modules', 'ember-animate', name + '-addon');
+  var treePath = path.join('node_modules', 'ember-animate', name + '-addon');
   var addon;
 
   if (fs.existsSync(treePath)) {
     addon = unwatchedTree(treePath);
   }
-
+  
   if (name === 'vendor') {
     ['velocity'].forEach(function(dep){
       var src = unwatchedTree(path.join('node_modules', 'ember-animate', 'vendor'));
-      addon = mergeTrees([addon, pickFiles(src, {srcDir: dep})]);
+      addon = mergeTrees([addon, pickFiles(src, {srcDir: dep, destDir: dep})]);
     });
   }
   
@@ -37,7 +37,7 @@ EmberAnimate.prototype.treeFor = function treeFor(name) {
 
 EmberAnimate.prototype.included = function included(app) {
   this.app = app;
-  //this.app.import('vendor/velocity/jquery.velocity.js');
+  this.app.import('vendor/velocity/jquery.velocity.js');
   this.app.import('vendor/animate/animate.css');  
 };
 
