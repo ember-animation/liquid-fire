469f89d0093227b215d276d410109d3acaf3e903
Edward Faulkner <ef@alum.mit.edu>
2014-07-07 14:43:13 -0400

    resolve promises on stop

--- app-addon/libs/liquid-fire/animate.js
+++ app-addon/libs/liquid-fire/animate.js
@@ -1,9 +1,8 @@
+import Promise from "./promise";
+
 // Wraps our calls to velocity.js so they always return a promise
-// (there's a PR in velocity upstream to add native promise support).
-//
-// In general, our API uses promises over the built-in queues provided
-// by animation libraries because promises are more composable and
-// general purpose across animation methods.
+// (there's a PR in velocity upstream to add native promise support --
+// once that's ready we can eliminate a lot of this).
 export function animate(view, props, opts) {
   return new Promise(function(resolve) {
     var elt;
@@ -14,15 +13,35 @@ export function animate(view, props, opts) {
     if (!opts) {
       opts = {};
     }
-    opts.complete = resolve;
+
+    // This is needed for now because velocity doesn't have a callback
+    // that fires after `stop`.
+    if (!view._velocityAnimations) {
+      view._velocityAnimations = [];
+    }
+    view._velocityAnimations.push(resolve);
+
+    opts.complete = function(){
+      var i = view._velocityAnimations.indexOf(resolve);
+      if (i >=0 ) {
+        view._velocityAnimations.splice(i, 1);
+      }
+      resolve();
+    };
     elt.velocity(props, opts);
   });
 }
 
 export function stop(view) {
-  var elt;
+  var elt, animList;
   if (view && (elt = view.$())) {
-    elt.velocity('stop', true);
+    elt.velocity('stop', true);    
+    if (animList = view._velocityAnimations) {
+      for (var i=0; i < animList.length; i++) {
+        animList[i]();
+      }
+      view._velocityAnimations = [];
+    }
   }
 }
 
