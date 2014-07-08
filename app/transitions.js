3f5e49495001d74fb0017d28312b7bb98a0e4b11
Edward Faulkner <ef@alum.mit.edu>
2014-07-08 00:15:32 -0400

    progress

--- app/transitions.js
+++ app/transitions.js
@@ -1,8 +1,8 @@
 import { Transitions } from "./libs/liquid-fire";
 
 export default Transitions.map(function(){
-  this.from('index').to('second').use('toRight');
-  this.from('second').to('index').use('toLeft');
-  this.from('empty').to('index').use('crossFade');
+  this.from('test-outlet.index').to('test-outlet.second').use('toRight');
+  this.from('test-outlet.second').to('test-outlet.index').use('toLeft');
+  this.from('empty').to('test-outlet.index').use('crossFade');
   this.from('default').to('default').use('toRight');
 });
