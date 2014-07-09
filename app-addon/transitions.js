1c70d2b445c7d1030bab2b66062616591da21219
Edward Faulkner <ef@alum.mit.edu>
2014-07-09 16:04:03 -0400

    fix stub transition config file

--- app-addon/transitions.js
+++ app-addon/transitions.js
@@ -1,5 +1,5 @@
 // This is an empty stub, the client application should introduce
 // their own version of this file and use it to configure their
 // transitions.
-import Transitions from "./libs/liquid-fire";
-export default Transitions.map(function(){});
+
+export default function(){};
