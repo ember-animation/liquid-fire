1c580a8505db9fd854ed585db3a78cef6cc04c13
Edward Faulkner <ef@alum.mit.edu>
2014-07-16 22:21:10 -0400

    Use velocity's {display: auto}

--- app-addon/libs/liquid-fire/animate.js
+++ app-addon/libs/liquid-fire/animate.js
@@ -14,12 +14,29 @@ export function animate(view, props, opts) {
       opts = {};
     }
 
+    // By default, we ask velocity to reveal the elements at the start
+    // of animation. Our animated divs are all initially rendered at
+    // display:none to prevent a flash of before-animated content.
+    //
+    // At present, velocity's 'auto' just picks a value for the css
+    // display property based on the element type. I have a PR that
+    // would let it defer to the stylesheets instead.
+    if (typeof(opts.display) === 'undefined') {
+      opts.display = 'auto';
+    }
+
     // This is needed for now because velocity doesn't have a callback
     // that fires after `stop`.
     if (!view._velocityAnimations) {
       view._velocityAnimations = [];
     }
     view._velocityAnimations.push(resolve);
+
+    opts.begin = function(elements){
+      for (var i=0; i<elements.length; i++) {
+        elements[i].style.display = "";
+      }
+    }
 
     opts.complete = function(){
       var i = view._velocityAnimations.indexOf(resolve);
@@ -28,7 +45,6 @@ export function animate(view, props, opts) {
       }
       resolve();
     };
-    elt.show();
     elt.velocity(props, opts);
   });
 }
