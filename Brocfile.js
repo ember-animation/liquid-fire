b5ef828d3b4ac34b7a97bc307b3277912518c58d
Edward Faulkner <ef@alum.mit.edu>
2014-07-17 20:49:26 -0400

    navlinks with glyphicons

--- Brocfile.js
+++ Brocfile.js
@@ -1,6 +1,7 @@
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var mergeTrees = require('broccoli-merge-trees');
+var pickFiles = require('broccoli-static-compiler');
 
 var appTree    = mergeTrees(['app-addon', 'app'], { overwrite: true });
 var templateTree    = mergeTrees(['app-addon/templates', 'app/templates'], { overwrite: true });
@@ -17,4 +18,9 @@ var app = new EmberApp({
 app.import("vendor/velocity/jquery.velocity.js");
 app.import("vendor/liquid-fire/liquid-fire.css");
 
-module.exports = app.toTree();
+var fonts = pickFiles('vendor/bootstrap-sass-official/assets/fonts', {
+  srcDir:'/',
+  destDir: 'assets'
+});
+
+module.exports = mergeTrees([app.toTree(), fonts]);
--- app/components/nav-link.js
+++ app/components/nav-link.js
@@ -1,3 +1,19 @@
 export default Ember.Component.extend({
-  isForward: Ember.computed.equal('direction', 'forward')
+  classNames: ['nav-link'],
+  classNameBindings: ['direction'],
+
+  preGlyphicon: function(){
+    if (this.get('topic') && this.get('direction') === 'back') {
+      return 'glyphicon-chevron-left';
+    }
+  }.property('direction', 'topic'),
+
+  postGlyphicon: function(){
+    if (this.get('topic') && this.get('direction') === 'forward') {
+      return 'glyphicon-chevron-right';
+    }
+  }.property('direction', 'topic')
+
+
+
 });
