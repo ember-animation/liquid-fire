854d11f92c31f5a50416c86b6598653d6b5bcfa6
Edward Faulkner <ef@alum.mit.edu>
2014-07-21 17:39:26 -0400

    progress on liquid-box documentation

--- app/transitions.js
+++ app/transitions.js
@@ -30,7 +30,14 @@ export default function(){
   );
 
   this.transition(
-    this.between({'class': 'vehicles'}),
+    // TODO: this case needs a friendlier API shortcut.
+    //
+    //If we just said between({class: 'vehicles'}) we'd get an
+    // unintended animation at the first render.
+    this.fromContext(function(change){
+      return typeof(change.leaving.context) !== "undefined";
+    }),
+    this.toContext({'class': 'vehicles'}),
     this.use('crossFade')
   );
 
