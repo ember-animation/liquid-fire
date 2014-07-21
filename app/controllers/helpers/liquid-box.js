18302714d0e1061f7007ec3c3eecc12699e47829
Edward Faulkner <ef@alum.mit.edu>
2014-07-21 11:01:19 -0400

    states derp derp

--- app/controllers/helpers/liquid-box.js
+++ app/controllers/helpers/liquid-box.js
@@ -3,6 +3,6 @@ import Ember from "ember";
 export default Ember.Controller.extend({
   vehicle: 'bike',
   vehicles: ['bike', 'car'],
-  states: ['', 'AL', 'AK', 'AZ', 'AR'],
+  states: ['', 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT'],
   isBike: Ember.computed.equal('vehicle', 'bike')
 });
