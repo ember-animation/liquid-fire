74e43f51ed89906b859b8243add91bbec719535a
Edward Faulkner <ef@alum.mit.edu>
2014-07-23 11:29:08 -0400

    remove old test routes

--- app/router.js
+++ app/router.js
@@ -18,16 +18,6 @@ Router.map(function() {
     this.resource('helpers.liquid-measure', { path: 'liquid-measure'});
     this.resource('helpers.liquid-box', { path: 'liquid-box'});
   });
-
-  this.resource("test-outlet", function(){
-    this.route('second');
-    this.route('third', {path: '/third/:id'});
-  });
-  this.route("test-with", {path: "/test-with/:id"});
-  this.route("test-bind", {path: "/test-bind"});
-  this.route("test-measure", {path: "/test-measure"});
-  this.route("test-box", {path: "/test-box"});
-  this.route("test-if", {path: "/test-if"});
 });
 
 export default Router;
