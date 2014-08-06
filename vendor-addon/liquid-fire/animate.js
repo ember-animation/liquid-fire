f678065b3a7639cea9a9cf48a6b7f8a301e0e196
Edward Faulkner <ef@alum.mit.edu>
2014-08-06 17:01:50 -0400

    rely on velocity's own promises (closes #4)

--- vendor-addon/liquid-fire/animate.js
+++ vendor-addon/liquid-fire/animate.js
@@ -1,69 +1,43 @@
+/* global $ */
 import Promise from "./promise";
 
-// Wraps our calls to velocity.js so they always return a promise
-// (there's a PR in velocity upstream to add native promise support --
-// once that's ready we can eliminate a lot of this).
+// Make sure Velocity always has promise support by injecting our own
+// RSVP-based implementation if it doesn't already have one.
+if (!$.Velocity.Promise) {
+  $.Velocity.Promise = Promise;
+}
+
 export function animate(view, props, opts) {
-  return new Promise(function(resolve) {
-    var elt;
-    if (!view || !(elt = view.$())) {
-      resolve();
-      return;
-    }
-    if (!opts) {
-      opts = {};
-    }
+  var elt;
 
-    // By default, we ask velocity to reveal the elements at the start
-    // of animation. Our animated divs are all initially rendered at
-    // display:none to prevent a flash of before-animated content.
-    //
-    // At present, velocity's 'auto' just picks a value for the css
-    // display property based on the element type. I have a PR that
-    // would let it defer to the stylesheets instead.
-    if (typeof(opts.display) === 'undefined') {
-      opts.display = 'auto';
-    }
+  if (!view || !(elt = view.$()) || !elt[0]) {
+    return Promise.cast();
+  }
 
-    // This is needed for now because velocity doesn't have a callback
-    // that fires after `stop`.
-    if (!view._velocityAnimations) {
-      view._velocityAnimations = [];
-    }
-    view._velocityAnimations.push(resolve);
+  if (!opts) {  opts = {}; }
 
-    opts.begin = function(elements){
-      for (var i=0; i<elements.length; i++) {
-        elements[i].style.display = "";
-      }
-    };
+  // By default, we ask velocity to reveal the elements at the start
+  // of animation. Our animated divs are all initially rendered at
+  // display:none to prevent a flash of before-animated content.
+  //
+  // At present, velocity's 'auto' just picks a value for the css
+  // display property based on the element type. I have a PR that
+  // would let it defer to the stylesheets instead.
+  if (typeof(opts.display) === 'undefined') {
+    opts.display = 'auto';
+  }
 
-    opts.complete = function(){
-      var i = view._velocityAnimations.indexOf(resolve);
-      if (i >=0 ) {
-        view._velocityAnimations.splice(i, 1);
-      }
-      resolve();
-    };
-    elt.velocity(props, opts);
-  });
+  return $.Velocity.animate(elt[0], props, opts);
 }
 
 export function stop(view) {
-  var elt, animList;
+  var elt;
   if (view && (elt = view.$())) {
     elt.velocity('stop', true);
-    if (animList = view._velocityAnimations) {
-      for (var i=0; i < animList.length; i++) {
-        animList[i]();
-      }
-      view._velocityAnimations = [];
-    }
   }
 }
 
 export function setDefaults(props) {
-  /* global $ */
   for (var key in props) {
     if (props.hasOwnProperty(key)) {
       $.Velocity.defaults[key] = props[key];
