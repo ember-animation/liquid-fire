3ff9e1f8461c999ae978f043976481f7bc6d1a57
Edward Faulkner <ef@alum.mit.edu>
2014-08-07 01:11:00 -0400

    load transition map via container

--- app-addon/initializers/liquid-fire.js
+++ app-addon/initializers/liquid-fire.js
@@ -1,10 +1,9 @@
 import { initialize } from "vendor/liquid-fire";
-import transitionConfig from "../transitions";
 
 export default {
   name: 'liquid-fire',
 
   initialize: function(container) {
-    initialize(container, transitionConfig);
+    initialize(container, container.lookupFactory('transitions:main'));
   }
 };
