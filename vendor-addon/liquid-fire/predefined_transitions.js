ac575d4b54fa674c7cc1b209530bad5bbed26e15
Edward Faulkner <ef@alum.mit.edu>
2014-08-07 16:18:57 -0400

    minor edits

--- vendor-addon/liquid-fire/predefined_transitions.js
+++ vendor-addon/liquid-fire/predefined_transitions.js
@@ -64,19 +64,4 @@ export default function predefinedTransitions(){
   });
   // END-SNIPPET
 
-  this.define('rotateBelow', function(oldView, insertNewView, opts) {
-    var direction = 1;
-    if (opts && opts.direction === 'cw') {
-      direction = -1;
-    }
-    stop(oldView);
-    return insertNewView().then(function(newView) {
-      oldView.$().css('transform-origin', '50% 150%');
-      newView.$().css('transform-origin', '50% 150%');
-      return Promise.all([
-        animate(oldView, { rotateZ: -90*direction + 'deg' }, opts),
-        animate(newView, { rotateZ: ['0deg', 90*direction+'deg'] }, opts),
-      ]);
-    });
-  });
 }
