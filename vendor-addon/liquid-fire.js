0b7515e97c941de909b35ff26273a370cadf07c0
Edward Faulkner <ef@alum.mit.edu>
2014-08-07 02:58:13 -0400

    expose some new animation primitives

--- vendor-addon/liquid-fire.js
+++ vendor-addon/liquid-fire.js
@@ -1,6 +1,7 @@
 import Transitions from "./liquid-fire/transitions";
-import { animate, stop } from "./liquid-fire/animate";
+import { animate, stop, isAnimating, finish } from "./liquid-fire/animate";
 import Promise from "./liquid-fire/promise";
 import initialize from "./liquid-fire/initialize";
 import MutationObserver from "./liquid-fire/mutation-observer";
-export { Transitions, animate, stop, Promise, initialize, MutationObserver };
+export { Transitions, animate, stop, isAnimating, finish,
+         Promise, initialize, MutationObserver };
