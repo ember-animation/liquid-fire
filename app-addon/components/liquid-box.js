7c1cd12c990ef224a149c4e037dfd5af3e94d8e3
Edward Faulkner <ef@alum.mit.edu>
2014-08-14 20:37:26 -0400

    defend against un-dimensions initial content measurement

--- app-addon/components/liquid-box.js
+++ app-addon/components/liquid-box.js
@@ -6,13 +6,21 @@ export default Ember.Component.extend({
   duration: 250,
   trackWidth: true,
   trackHeight: true,
+  _enabled: false,
 
   initialSize: function(){
     var elt = this.$();
     var measure = this._childViews[0];
     measure.updateMeasurements();
     elt.css(this.targetDimensions(measure.get('width'), measure.get('height')));
+    Ember.run.later(this, 'enable', this.get('duration'));
   }.on('didInsertElement'),
+
+  // This causes us to absorb any size changes that happen during the
+  // first `duration` after insertion without animating them.
+  enable: function(){
+    this.set('_enabled', true);
+  },
 
   resize: Ember.observer('contentHeight', 'contentWidth', function() {
     Ember.run.debounce(this, '_resize', 100);
@@ -34,7 +42,7 @@ export default Ember.Component.extend({
     stop(this);
     animate(this,
             this.targetDimensions(this.get('contentWidth'), this.get('contentHeight')),
-            { duration: this.get('duration') }
+            { duration: this.get('_enabled') ? this.get('duration') : 0}
            );
   }
 });
