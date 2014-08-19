44dec4acd2478683a4970369121c52a0ce0c299c
Edward Faulkner <ef@alum.mit.edu>
2014-08-18 22:27:42 -0400

    More graceful interruptions for move-over
    Related to #40

--- app-addon/transitions/move-over.js
+++ app-addon/transitions/move-over.js
@@ -1,18 +1,25 @@
-import { stop, animate, Promise } from "vendor/liquid-fire";
+import { stop, animate, Promise, isAnimating, finish } from "vendor/liquid-fire";
 
 export default function moveOver(oldView, insertNewView, dimension, direction, opts) {
   var property  = 'translate' + dimension.toUpperCase(),
       oldParams = {},
-      newParams = {};
+      newParams = {},
+      firstStep;
+
+  if (isAnimating(oldView, 'moving-in')) {
+    firstStep = finish(oldView, 'moving-in');
+  } else {
+    stop(oldView);
+    firstStep = Promise.cast();
+  }
 
   oldParams[property] = (100 * direction) + '%';
   newParams[property] = ["0%", (-100 * direction) + '%'];
 
-  stop(oldView);
-  return insertNewView().then(function(newView){
+  return firstStep.then(insertNewView).then(function(newView){
     return Promise.all([
       animate(oldView, oldParams, opts),
-      animate(newView, newParams, opts)
+      animate(newView, newParams, opts, 'moving-in')
     ]);
   });
 }
