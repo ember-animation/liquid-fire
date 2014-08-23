05c04ec82a0d9d486bacde2cace3ec3d31307137
Edward Faulkner <ef@alum.mit.edu>
2014-08-23 14:16:56 -0400

    switch to newer nested-route syntax

--- app/router.js
+++ app/router.js
@@ -7,14 +7,14 @@ var Router = Ember.Router.extend({
 Router.map(function() {
 
   this.resource("helpers-documentation", { path: 'helpers'}, function(){
-    this.resource('helpers-documentation.liquid-outlet', { path: 'liquid-outlet'}, function(){
+    this.route('liquid-outlet', function(){
       this.route('other');
     });
-    this.resource('helpers-documentation.liquid-with', { path: 'liquid-with'}, function(){
+    this.route('liquid-with', function(){
       this.route('page', { path: '/:id' });
     });
-    this.resource('helpers-documentation.liquid-bind', { path: 'liquid-bind'});
-    this.resource('helpers-documentation.liquid-if', { path: 'liquid-if'});
+    this.route('liquid-bind');
+    this.route('liquid-if');
   });
 
   this.resource('transition-map', function(){
@@ -23,14 +23,16 @@ Router.map(function() {
     this.route('dom-constraints');
     this.route('choosing-transitions');
   });
+
   this.resource('transitions', function(){
     this.route('predefined');
     this.route('defining');
-    this.resource('transitions.primitives', { path: 'primitives'}, function(){
+    this.route('primitives', function(){
       this.route('two');
       this.route('three');
     });
   });
+
 });
 
 export default Router;
