dc59d52af710d9fd30c5cb945af2627ca0a93936
Edward Faulkner <ef@alum.mit.edu>
2014-08-22 21:40:43 -0400

    closes #29

--- app-addon/initializers/liquid-fire.js
+++ app-addon/initializers/liquid-fire.js
@@ -1,9 +1,15 @@
 import { initialize } from "vendor/liquid-fire";
+import Ember from "ember";
 
 export default {
   name: 'liquid-fire',
 
   initialize: function(container) {
     initialize(container, container.lookupFactory('transitions:main'));
+    if (Ember.testing) {
+      Ember.Test.registerWaiter(function(){
+        return container.lookup('transitions:map').runningTransitions() === 0;
+      });
+    }
   }
 };
