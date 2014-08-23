e762141755defa0a53c92460690a67350db7fed4
Edward Faulkner <ef@alum.mit.edu>
2014-08-22 23:34:26 -0400

    fix unintended url structure

--- app/router.js
+++ app/router.js
@@ -15,19 +15,20 @@ Router.map(function() {
     });
     this.resource('helpers-documentation.liquid-bind', { path: 'liquid-bind'});
     this.resource('helpers-documentation.liquid-if', { path: 'liquid-if'});
-    this.resource('transition-map', function(){
-      this.route('route-constraints');
-      this.route('model-constraints');
-      this.route('dom-constraints');
-      this.route('choosing-transitions');
-    });
-    this.resource('transitions', function(){
-      this.route('predefined');
-      this.route('defining');
-      this.resource('transitions.primitives', { path: 'primitives'}, function(){
-        this.route('two');
-        this.route('three');
-      });
+  });
+
+  this.resource('transition-map', function(){
+    this.route('route-constraints');
+    this.route('model-constraints');
+    this.route('dom-constraints');
+    this.route('choosing-transitions');
+  });
+  this.resource('transitions', function(){
+    this.route('predefined');
+    this.route('defining');
+    this.resource('transitions.primitives', { path: 'primitives'}, function(){
+      this.route('two');
+      this.route('three');
     });
   });
 });
