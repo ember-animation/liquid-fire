c71b5f58fdf794573d664eab3c68879b2c2cb526
Edward Faulkner <ef@alum.mit.edu>
2014-10-13 17:28:19 -0400

    cleanup previous documentation edit

--- app/controllers/modal-documentation.js
+++ app/controllers/modal-documentation.js
@@ -1,7 +1,8 @@
 import Ember from "ember";
-
+// BEGIN-SNIPPET modal-controller
 export default Ember.Controller.extend({
   queryParams: ['salutation', 'person'],
   salutation: null,
   person: null
 });
+// END-SNIPPET
