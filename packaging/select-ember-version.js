47e0b20d0abb5d204679169537f0498816cb1a4f
Edward Faulkner <ef@alum.mit.edu>
2014-11-18 21:35:48 -0500

    updates to match new source structure

--- packaging/select-ember-version.js
+++ packaging/select-ember-version.js
@@ -19,14 +19,14 @@ function maybeChangeVersion(channel) {
 }
 
 function logVersion(channel) {
-  var got = require(path.join(__dirname, '..', 'vendor', 'ember', 'bower.json')).version;
+  var got = require(path.join(__dirname, '..', 'bower_components', 'ember', 'bower.json')).version;
   console.log("Based on " + channel + " I'm using version " + got);
 }
 
 maybeChangeVersion(process.env.EMBER_CHANNEL).then(function(channel){
   logVersion(channel);
   process.exit(0);
-}, function(err){
+}).catch(function(err){
   console.log(err);
   console.log(err.stack);
   process.exit(-1);
