70ed0ca90884448ff8e67d7e0a087a6ccc4f0fec
Edward Faulkner <ef@alum.mit.edu>
2014-11-18 22:27:21 -0500

    updating CI to build properly against all channels again

--- packaging/select-ember-version.js
+++ packaging/select-ember-version.js
@@ -9,22 +9,83 @@ function maybeChangeVersion(channel) {
   if (typeof(channel) === 'undefined') {
     return RSVP.Promise.resolve('existing');
   }
-
   var bowerFile = path.join(__dirname, '..', 'bower.json');
   return run('git', ['checkout', bowerFile]).then(function(){
-    var bowerContent = fs.readFileSync(bowerFile, 'utf-8');
-    fs.writeFileSync(bowerFile, bowerContent.replace(/canary/g, channel));
-    return run('bower', ['install'], {cwd: path.join(__dirname, '..')});
+    var bowerJSON = require(bowerFile);
+    fs.writeFileSync(bowerFile, JSON.stringify(rewrite(bowerJSON, channel), null, 2));
+    return run('bower', ['install'], {cwd: path.join(__dirname, '..')})
+      .then(chooseTemplateCompiler);
   }).then(function(){return channel;});
 }
 
-function logVersion(channel) {
-  var got = require(path.join(__dirname, '..', 'bower_components', 'ember', 'bower.json')).version;
-  console.log("Based on " + channel + " I'm using version " + got);
+function rewrite(bowerJSON, channel) {
+  if (channel === 'existing') {
+    return bowerJSON;
+  }
+
+  if (!bowerJSON.resolutions) {
+    bowerJSON.resolutions = {};
+  }
+
+  bowerJSON.dependencies.ember = "components/ember#" + channel;
+  bowerJSON.resolutions.ember = channel;
+
+  if (channel === 'release') {
+    bowerJSON.dependencies.handlebars = "1.3.0";
+    bowerJSON.resolutions.handlebars = "1.3.0";
+  } else {
+    bowerJSON.dependencies.handlebars = "2.0.0";
+    bowerJSON.resolutions.handlebars = "2.0.0";
+  }
+
+  return bowerJSON;
+}
+
+
+function chooseTemplateCompiler(channel) {
+  var state;
+
+  if (channel === 'existing') {
+    return RSVP.Promise.resolve();
+  }
+
+  if (channel === 'release') {
+    state = {
+      'broccoli-ember-hbs-template-compiler' : 'install',
+      'ember-cli-htmlbars' : 'uninstall'
+    };
+  } else {
+    state = {
+      'broccoli-ember-hbs-template-compiler' : 'uninstall',
+      'ember-cli-htmlbars' : 'install'
+    };
+  }
+  return RSVP.Promise.all(Object.keys(state).map(function(module){
+    return run('npm', [state[module], '--save-dev', module]);
+  }));
+}
+
+function foundVersion(package) {
+  var filename = path.join(__dirname, '..', 'bower_components', package, 'bower.json');
+  if (fs.existsSync(filename)) {
+    return require(filename).version;
+  }
+  filename = path.join(__dirname, '..', 'node_modules', package, 'package.json');
+  if (fs.existsSync(filename)) {
+    return require(filename).version;
+  }
+  return "none";
+}
+
+function logVersions(channel) {
+  console.log("Based on " + channel + " I'm using:");
+  ['ember', 'handlebars', 'broccoli-ember-hbs-template-compiler', 'ember-cli-htmlbars'].map(function(module){
+    console.log("  " + module + " " + foundVersion(module));
+  });
 }
 
 maybeChangeVersion(process.env.EMBER_CHANNEL).then(function(channel){
-  logVersion(channel);
+  logVersions(channel);
   process.exit(0);
 }).catch(function(err){
   console.log(err);
