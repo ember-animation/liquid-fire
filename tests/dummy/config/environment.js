20cfac0e9414eee5ac907c16d63241b7297b21a7
Edward Faulkner <ef@alum.mit.edu>
2014-11-18 22:30:30 -0500

    Revert "fix links from test page"
    This reverts commit 27dd0e3170bb185e385deed11b153d86983ae0c6.

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -34,7 +34,7 @@ module.exports = function(environment) {
   if (environment === 'test') {
     // Testem prefers this...
     ENV.baseURL = '/';
-    ENV.locationType = 'none';
+    ENV.locationType = 'auto';
 
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
