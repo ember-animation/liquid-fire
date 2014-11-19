8b24b886b843eb580337212ca125e05fe1a3b62f
Edward Faulkner <ef@alum.mit.edu>
2014-11-19 16:40:06 -0500

    move away from deprecated form of #with

--- tests/dummy/app/controllers/helpers-documentation/liquid-with/page.js
+++ tests/dummy/app/controllers/helpers-documentation/liquid-with/page.js
@@ -2,8 +2,7 @@ import Ember from "ember";
 
 export default Ember.ObjectController.extend({
   actions: {
-    higher: function(){
-      var model = this.get('model');
+    higher: function(model){
       this.transitionToRoute('helpers-documentation.liquid-with.page', model.get('id')+1);
     }
   }
