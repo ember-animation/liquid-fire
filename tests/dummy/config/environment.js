f89b8a6844bae188de002dc86e3a39739f0f0fad
Edward Faulkner <ef@alum.mit.edu>
2014-11-30 17:16:20 -0500

    ember-cli 0.1.3 init

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -34,7 +34,7 @@ module.exports = function(environment) {
   if (environment === 'test') {
     // Testem prefers this...
     ENV.baseURL = '/';
-    ENV.locationType = 'auto';
+    ENV.locationType = 'none';
 
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
