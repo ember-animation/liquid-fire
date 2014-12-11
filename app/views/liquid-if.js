45b7ccf78430925a7ac61f54edc60d733796352b
Kamal Mahyuddin <kamal.fariz@gmail.com>
2014-12-11 13:03:43 -0800

    on HTMLBars, Ember.Handlebars.VM.noop doesn't exist, fixes ef4/liquid-fire#140

--- app/views/liquid-if.js
+++ app/views/liquid-if.js
@@ -1,10 +1,12 @@
 import LiquidOutlet from "./liquid-outlet";
 import Ember from "ember";
 
+var isHTMLBars = !!Ember.HTMLBars;
+
 export default LiquidOutlet.extend({
   liquidUpdate: Ember.on('init', Ember.observer('showFirst', function(){
     var template = this.get('templates')[this.get('showFirst') ? 0 : 1];
-    if (!template || template === Ember.Handlebars.VM.noop) {
+    if (!template || !isHTMLBars && template === Ember.Handlebars.VM.noop) {
       this.set('currentView', null);
       return;
     }
