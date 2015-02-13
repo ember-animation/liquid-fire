8f533ad308621d0536c0aca66d7d82b37655bbec
Edward Faulkner <ef@alum.mit.edu>
2015-02-12 21:57:13 -0500

    ember 1.10 and later need a separate template compiler for tests

--- Brocfile.js
+++ Brocfile.js
@@ -14,6 +14,10 @@ var app = new EmberAddon({
 app.import('bower_components/moment/moment.js');
 app.import('vendor/sinon.js', { type: 'test'});
 
+if (!/^1\.[89]/.test(require('./bower_components/ember/bower.json').version)) {
+  app.import('bower_components/ember/ember-template-compiler.js', { type: 'test' });
+}
+
 var bootstrap = 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap';
 fs.readdirSync(bootstrap).forEach(function(font){
   app.import(path.join(bootstrap, font), { destDir: '/fonts/bootstrap'});
