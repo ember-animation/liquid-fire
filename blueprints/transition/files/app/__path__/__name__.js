5cbf31478b573789d8991697cc4aab033978cfd7
Edward Faulkner <ef@alum.mit.edu>
2015-02-12 22:49:36 -0500

    Change blueprint to have a runnable example.

--- blueprints/transition/files/app/__path__/__name__.js
+++ blueprints/transition/files/app/__path__/__name__.js
@@ -2,15 +2,15 @@ import { animate, stop } from "liquid-fire";
 
 export default function (oldView, insertNewView) {
   
-  // Example:
-  // First we stop any animation on the oldView. We then call Animate on the oldView, 
-  // which returns us a promise. Once the first step resolves we insertNewView and then 
-  // animate the new view.
+  // Stop any currently running animation on oldView
+  stop(oldView);
 
-  // stop(oldView);
-  // var firstStep = animate(oldView, {opacity: 0}, {duration: '10'}, 'fade-out');
-
-  // return firstStep.then(insertNewView).then(function(newView){
-  //   return animate(newView, {left: ['0px', '100px'], opacity: [1, 0]}, {easing: 'easeOutQuart'}, 'slide-in');
-  // });
+  // Fade out the old view
+  return animate(oldView, { opacity: 0 })
+  // Render the new view
+    .then(insertNewView)
+  // And fade it in, from opacity 0 to 1
+    .then(function(newView){
+      return animate(newView, { opacity: [1, 0]});
+    });
 }
