961d7cdfe49b2df783da9e14c3a916f2c8eb67d6
Edward Faulkner <ef@alum.mit.edu>
2015-02-16 01:15:02 -0500

    newer ember-cli doesn't need this

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -5,8 +5,3 @@ import {
 
 setResolver(resolver);
 
-document.write('<div id="ember-testing-container"><div id="ember-testing"></div></div>');
-
-QUnit.config.urlConfig.push({ id: 'nocontainer', label: 'Hide container' });
-var containerVisibility = QUnit.urlParams.nocontainer ? 'hidden' : 'visible';
-document.getElementById('ember-testing-container').style.visibility = containerVisibility;
