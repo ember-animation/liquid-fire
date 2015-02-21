734c5c16b30c2168c6de8b26505bdf7d62cd5b96
Edward Faulkner <ef@alum.mit.edu>
2015-02-21 03:14:27 -0500

    adding minimum ember version warning

--- app/initializers/liquid-fire.js
+++ app/initializers/liquid-fire.js
@@ -1,17 +1,31 @@
 import { initialize } from "liquid-fire";
 import Ember from "ember";
 
+var minEmberVersion = [1, 11];
+var minVelocityVersion = [0, 11, 8];
+
+function emberVersion() {
+  var m = /^(\d+)\.(\d+)/.exec(Ember.VERSION);
+  if (!m) {
+    return [ 0, 0 ];
+  }
+  return [ parseInt(m[1]), parseInt(m[2]) ];
+}
+
 export default {
   name: 'liquid-fire',
 
   initialize: function(container) {
+    if (Ember.compare(minEmberVersion, emberVersion()) === 1) {
+      Ember.warn(`This version of liquid fire requires Ember ${ minEmberVersion.join('.') } or newer`);
+    }
+
     if (!Ember.$.Velocity) {
       Ember.warn("Velocity.js is missing");
     } else {
       var version = Ember.$.Velocity.version;
-      var recommended = [0, 11, 8];
-      if (Ember.compare(recommended, [version.major, version.minor, version.patch]) === 1) {
-        Ember.warn("You should probably upgrade Velocity.js, recommended minimum is " + recommended.join('.'));
+      if (Ember.compare(minVelocityVersion, [version.major, version.minor, version.patch]) === 1) {
+        Ember.warn("You should probably upgrade Velocity.js, recommended minimum is " + minVelocityVersion.join('.'));
       }
     }
 
