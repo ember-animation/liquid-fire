33541aaa5480ca82ed11a1b6d344212e0b02baca
Edward Faulkner <ef@alum.mit.edu>
2015-02-25 22:44:40 -0500

    simplifying helpers

--- addon/ember-internals.js
+++ addon/ember-internals.js
@@ -6,3 +6,20 @@
 export function containingElement(view) {
   return view._morph.contextualElement;
 }
+
+export function makeHelperShim(componentName) {
+  return {
+    isHTMLBars: true,
+    helperFunction: function liquidFireHelper(params, hash, options, env) {
+      var componentLookup = this.container.lookup('component-lookup:main');
+      var cls = componentLookup.lookupFactory(componentName);
+      hash.value = params[0];
+      if (hash['class']) {
+        hash.innerClass = hash['class'];
+        delete hash['class'];
+      }
+      hash.tagName = "";
+      env.helpers.view.helperFunction.call(this, [cls], hash, options, env);
+    }
+  };
+}
--- app/helpers/liquid-bind.js
+++ app/helpers/liquid-bind.js
@@ -1,13 +1,2 @@
-export default {
-  isHTMLBars: true,
-  helperFunction: function liquidBindHelper(params, hash, options, env) {
-    var componentLookup = this.container.lookup('component-lookup:main');
-    var cls = componentLookup.lookupFactory('liquid-bind-c');
-    hash.value = params[0];
-    if (hash['class']) {
-      hash.innerClass = hash['class'];
-      delete hash['class'];
-    }
-    env.helpers.view.helperFunction.call(this, [cls], hash, options, env);
-  }
-};
+import { makeHelperShim } from "liquid-fire/ember-internals";
+export default makeHelperShim('liquid-bind-c');
