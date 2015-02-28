728a4339433fdcc063ea89ec9cf1569ff0c1acbf
Edward Faulkner <ef@alum.mit.edu>
2015-02-27 22:12:54 -0500

    whitespace

--- app/helpers/liquid-if.js
+++ app/helpers/liquid-if.js
@@ -1,15 +1,15 @@
 export default {
-    isHTMLBars: true,
-    helperFunction: function liquidFireHelper(params, hash, options, env) {
-      var componentLookup = this.container.lookup('component-lookup:main');
-      var cls = componentLookup.lookupFactory('liquid-if');
-      hash.value = params[0];
-      if (hash['class']) {
-        hash.innerClass = hash['class'];
-        delete hash['class'];
-      }
-      hash.tagName = "";
-      hash.inverseTemplate = options.inverse;
-      env.helpers.view.helperFunction.call(this, [cls], hash, options, env);
+  isHTMLBars: true,
+  helperFunction: function liquidFireHelper(params, hash, options, env) {
+    var componentLookup = this.container.lookup('component-lookup:main');
+    var cls = componentLookup.lookupFactory('liquid-if');
+    hash.value = params[0];
+    if (hash['class']) {
+      hash.innerClass = hash['class'];
+      delete hash['class'];
     }
+    hash.tagName = "";
+    hash.inverseTemplate = options.inverse;
+    env.helpers.view.helperFunction.call(this, [cls], hash, options, env);
+  }
 };
