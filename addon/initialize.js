c273f8adc5431295c74fa47639c03bf5ef24b419
Edward Faulkner <ef@alum.mit.edu>
2015-02-27 23:23:06 -0500

    dropping old code

--- addon/initialize.js
+++ addon/initialize.js
@@ -3,14 +3,7 @@ import Modals from "./modals";
 
 export function initialize(application) {
   application.register('transitions:map', TransitionMap);
-
-  ['outlet', 'with', 'if'].forEach(function(viewName) {
-    application.inject('view:liquid-' + viewName, 'transitionMap', 'transitions:map');
-  });
-
   application.inject('component:liquid-versions', 'transitionMap', 'transitions:map');
-
   application.register('liquid-modals:main', Modals);
   application.inject('component:liquid-modal', 'owner', 'liquid-modals:main');
-
 }
