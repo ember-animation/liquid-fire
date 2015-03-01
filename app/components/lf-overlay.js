04a452f9ebfe699851df31f48c5fc4907f8423f3
Edward Faulkner <ef@alum.mit.edu>
2015-03-01 12:19:15 -0500

    avoid racing the lf-overlay body class

--- app/components/lf-overlay.js
+++ app/components/lf-overlay.js
@@ -1,13 +1,26 @@
 import Ember from "ember";
+var COUNTER = '__lf-modal-open-counter';
+
 export default Ember.Component.extend({
   tagName: 'span',
   classNames: ['lf-overlay'],
+
   didInsertElement: function() {
-    Ember.$('body').addClass('lf-modal-open');
+    var body = Ember.$('body');
+    var counter = body.data(COUNTER) || 0;
+    body.addClass('lf-modal-open');
+    body.data(COUNTER, counter+1);
   },
+
   willDestroy: function() {
-    Ember.$('body').removeClass('lf-modal-open');
+    var body = Ember.$('body');
+    var counter = body.data(COUNTER) || 0;
+    body.data(COUNTER, counter-1);
+    if (counter < 2) {
+      body.removeClass('lf-modal-open');
+    }
   },
+
   click: function() {
     this.sendAction('clickAway');
   }
