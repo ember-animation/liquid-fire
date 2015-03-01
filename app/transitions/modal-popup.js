4525137347b0ee4f15b2cd543859299238cbe94a
Edward Faulkner <ef@alum.mit.edu>
2015-03-01 14:16:02 -0500

    extensible modal-popup transition

--- app/transitions/modal-popup.js
+++ app/transitions/modal-popup.js
@@ -1,54 +1,34 @@
-import Ember from "ember";
-import { Promise } from "liquid-fire";
-var Velocity = Ember.$.Velocity;
+import { animate, explode, Promise } from "liquid-fire";
 
-function hideModal(oldElement) {
-  var box, obscure;
-  if (!oldElement ||
-      !(box = oldElement.find('.lm-container > div')) ||
-      !(box = box[0]) ||
-      !(obscure = oldElement.find('.lf-overlay')) ||
-      !(obscure = obscure[0])) {
-    return Promise.resolve();
-  }
-
+// The default transition for animating the background overlay element
+// in and out.
+function animateOverlay() {
   return Promise.all([
-    Velocity.animate(obscure, {opacity: [0, 0.5]}, {duration: 250}),
-    Velocity.animate(box, {scale: [0, 1]}, {duration: 250})
+    animate(this.oldElement, {opacity: [0, 0.5]}, {duration: 250}),
+    animate(this.newElement, {opacity: [0.5, 0]}, {duration: 250, display: ''})
   ]);
 }
 
-function revealModal(newElement) {
-  var box, obscure;
-  if (!newElement ||
-      !(box = newElement.find('.lm-container > div')[0]) ||
-      !(obscure = newElement.find('.lf-overlay')[0])) {
-    return Promise.resolve();
-  }
-
-  // we're not going to animate the whole view, rather we're going
-  // to animate two pieces of it separately. So we move the view
-  // properties down onto the individual elements, so that the
-  // animate function can reveal them at precisely the right time.
-  Ember.$(box).css({
-    display: 'none'
-  });
-
-  Ember.$(obscure).css({
-    display: 'none'
-  });
-  newElement.css({
-    visibility: ''
-  });
-
+// The default transition for animating the dialog box in and out.
+function animateDialog() {
   return Promise.all([
-    Velocity.animate(obscure, {opacity: [0.5, 0]}, {duration: 250, display: ''}),
-    Velocity.animate(box, {scale: [1, 0]}, {duration: 250, display: ''})
+    animate(this.oldElement, {scale: [0, 1]}, {duration: 250}),
+    animate(this.newElement, {scale: [1, 0]}, {duration: 250, display: ''})
   ]);
 }
 
-export default function modalPopup() {
-  return hideModal(this.oldElement).then(() => {
-    return revealModal(this.newElement);
-  });
+export default function modalPopup(opts={}) {
+  var [overlay, box] = explode(this, [
+    '.lf-overlay',
+    '.lm-container > div'
+  ]);
+
+  var overlayAnimation = opts.overlay ? this.lookup(opts.overlay) : animateOverlay;
+  var boxAnimation = opts.box ? this.lookup(opts.box) : animateDialog;
+
+  return Promise.all([
+    overlayAnimation.call(overlay),
+    boxAnimation.call(box)
+  ]);
+
 }
