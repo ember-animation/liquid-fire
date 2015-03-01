71d55e25b65d889bd893416c6eae2014525f3eda
Edward Faulkner <ef@alum.mit.edu>
2015-03-01 14:15:08 -0500

    export the explode util

--- addon/index.js
+++ addon/index.js
@@ -1,8 +1,8 @@
 import TransitionMap from "./transition-map";
-import { animate, stop, isAnimating, timeSpent, timeRemaining, finish } from "./animate";
+import { animate, stop, isAnimating, timeSpent, timeRemaining, finish, explode } from "./animate";
 import Promise from "./promise";
 import { initialize } from "./initialize";
 import MutationObserver from "./mutation-observer";
 import "./router-dsl-ext";
 export { TransitionMap, animate, stop, isAnimating, timeSpent, timeRemaining, finish,
-         Promise, initialize, MutationObserver };
+         explode, Promise, initialize, MutationObserver };
