79d24723a4d8a9ad5a9446419925cc43c3a00568
Edward Faulkner <ef@alum.mit.edu>
2015-03-02 01:26:11 -0500

    dropping color handling from fly-to
    it breaks too easily and isn't super useful

--- app/transitions/fly-to.js
+++ app/transitions/fly-to.js
@@ -15,19 +15,11 @@ export default function flyTo(opts={}) {
     translateX: newOffset.left - oldOffset.left,
     translateY: newOffset.top - oldOffset.top,
     width: this.newElement.width(),
-    height: this.newElement.height(),
-    backgroundColor: hexColor(this.newElement.css('background-color'))
+    height: this.newElement.height()
   };
 
   this.newElement.css({ visibility: 'hidden' });
   return animate(this.oldElement, motion, opts).then(() => {
     this.newElement.css({ visibility: ''});
   });
-}
-
-function hexColor(rgbColor) {
-  return '#' +
-    /(\d+)[^\d]+(\d+)[^\d]+(\d+)/.exec(rgbColor).slice(1).map(
-      (c) => ('0' + parseInt(c, 10).toString(16)).slice(-2)
-    ).join('');
 }
