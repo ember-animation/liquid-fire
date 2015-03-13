4106c963d6ce3cc2e3699344b491461b0dd3918c
Edward Faulkner <ef@alum.mit.edu>
2015-03-13 15:07:41 -0400

    provide default opts pojo

--- app/transitions/cross-fade.js
+++ app/transitions/cross-fade.js
@@ -1,6 +1,6 @@
 // BEGIN-SNIPPET cross-fade-definition
 import { animate, stop, Promise } from "liquid-fire";
-export default function crossFade(opts) {
+export default function crossFade(opts={}) {
   stop(this.oldElement);
   return Promise.all([
     animate(this.oldElement, {opacity: 0}, opts),
