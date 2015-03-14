56feb1b2ab957ca493e570b89c1baaf89613597d
Edward Faulkner <ef@alum.mit.edu>
2015-03-14 19:29:28 -0400

    add growth animations to the running transition count

--- addon/growable.js
+++ addon/growable.js
@@ -7,13 +7,18 @@ export default Ember.Mixin.create({
   growPixelsPerSecond: 200,
   growEasing: 'slide',
 
+  transitionMap: Ember.inject.service('liquid-fire-transitions'),
+
   animateGrowth: function(elt, have, want) {
+    this.transitionMap.activeCount++;
     return Promise.all([
       this._adaptDimension(elt, 'width', have, want),
       this._adaptDimension(elt, 'height', have, want)
-    ]);
+    ]).then(()=>{
+      this.transitionMap.activeCount--;
+    });
   },
-  
+
   _adaptDimension: function(elt, dimension, have, want) {
     var target = {};
     target[dimension] = [
@@ -23,12 +28,12 @@ export default Ember.Mixin.create({
     return Ember.$.Velocity(elt[0], target, {
       duration: this._durationFor(have[dimension], want[dimension]),
       queue: false,
-      easing: this.get('growEasing')      
+      easing: this.get('growEasing')
     });
   },
 
   _durationFor: function(before, after) {
     return Math.min(this.get('growDuration'), 1000*Math.abs(before - after)/this.get('growPixelsPerSecond'));
   }
-  
+
 });
