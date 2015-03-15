8bd97828c40e20b117d24f8ba17482445eae701a
Edward Faulkner <ef@alum.mit.edu>
2015-03-15 00:09:27 -0400

    robust mult-interruption support for fade transition

--- app/transitions/fade.js
+++ app/transitions/fade.js
@@ -3,14 +3,16 @@ import { isAnimating, finish, timeSpent, animate, stop } from "liquid-fire";
 export default function fade(opts={}) {
   var firstStep;
   var outOpts = opts;
+  var fadingElement = findFadingElement(this);
 
-  if (isAnimating(this.oldElement, 'fade-out')) {
-    // if the old view is already fading out, let it finish.
-    firstStep = finish(this.oldElement, 'fade-out');
+  if (fadingElement) {
+    // We still have some older version that is in the process of
+    // fading out, so out first step is waiting for it to finish.
+    firstStep = finish(fadingElement, 'fade-out');
   } else {
     if (isAnimating(this.oldElement, 'fade-in')) {
-      // if the old view is partially faded in, scale its fade-out
-      // duration appropriately.
+      // if the previous view is partially faded in, scale its
+      // fade-out duration appropriately.
       outOpts = { duration: timeSpent(this.oldElement, 'fade-in') };
     }
     stop(this.oldElement);
@@ -22,3 +24,15 @@ export default function fade(opts={}) {
   });
 }
 // END-SNIPPET
+
+function findFadingElement(context) {
+  for (var i = 0; i < context.older.length; i++) {
+    var entry = context.older[i];
+    if (isAnimating(entry.element, 'fade-out')) {
+      return entry.element;
+    }
+  }
+  if (isAnimating(context.oldElement, 'fade-out')) {
+    return context.oldElement;
+  }
+}
