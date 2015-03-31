2875a1f5e3c79dc2333297640bfb7a0d162892ce
Edward Faulkner <ef@alum.mit.edu>
2015-03-30 23:57:14 -0400

    adding tests for different box-sizing modes

--- tests/integration/transitions/explode-test.js
+++ tests/integration/transitions/explode-test.js
@@ -12,7 +12,7 @@ moduleForIntegration('Integration: explode transition', {
   }
 });
 
-test("it doesn't runs parts with no matching elements", function(assert) {
+test(`it doesn't runs parts with no matching elements`, function(assert) {
   expect(0);
   this.container.lookup('service:liquid-fire-transitions').map(function() {
     this.transition(
@@ -27,11 +27,11 @@ test("it doesn't runs parts with no matching elements", function(assert) {
   });
   this.render(`
               {{#liquid-if showBlue class="explode-transition-test"}}
-                <div class="bluebox"></div>
+              <div class="bluebox"></div>
               {{else}}
-                <div class="redbox"></div>
+              <div class="redbox"></div>
               {{/liquid-if}}
-  `);
+              `);
   this.set('showBlue', true);
 });
 
@@ -51,104 +51,107 @@ test("it matches the background", function(assert) {
   });
   this.render(`
               {{#liquid-if showBlue class="explode-transition-test"}}
-                <div class="bluebox"></div>
+              <div class="bluebox"></div>
               {{else}}
-                <div class="redbox"></div>
+              <div class="redbox"></div>
               {{/liquid-if}}
-  `);
+              `);
   this.set('showBlue', true);
 });
 
+['border-box', 'content-box'].forEach(function(boxSizing) {
 
-test('it avoids a jump at start of animation, with absolutely position elements', function(assert) {
-  var didTransition = false;
-  this.container.lookup('service:liquid-fire-transitions').map(function() {
-    this.transition(
-      this.hasClass('explode-transition-test'),
-      this.use('explode', {
-        pickOld: '.redbox',
-        pickNew: '.bluebox',
-        use: function() {
-          // sanity checks
-          assert.equal(this.oldElement && this.oldElement.length, 1, 'found old element');
-          assert.equal(this.newElement && this.newElement.length, 1, 'found new element');
-          assert.equal(this.oldElement && this.oldElement.css('background-color'), "rgb(255, 0, 0)");
+  test(`it avoids a jump at start of animation, with absolutely positioned elements (${boxSizing})`, function(assert) {
+    var didTransition = false;
+    this.container.lookup('service:liquid-fire-transitions').map(function() {
+      this.transition(
+        this.hasClass('explode-transition-test'),
+        this.use('explode', {
+          pickOld: '.redbox',
+          pickNew: '.bluebox',
+          use: function() {
+            // sanity checks
+            assert.equal(this.oldElement && this.oldElement.length, 1, 'found old element');
+            assert.equal(this.newElement && this.newElement.length, 1, 'found new element');
+            assert.equal(this.oldElement && this.oldElement.css('background-color'), "rgb(255, 0, 0)");
 
-          // the explode transition actually animates a copy of the
-          // original oldElement, which we can still find inside a
-          // liquid-child (the copy is not inside a liquid-child, that
-          // is part of the point of explode).
-          var realOldElement = this.oldElement.parent().find('.liquid-child .redbox');
-          assert.equal(realOldElement.length, 1, 'found actual old element');
-          assert.equal(realOldElement.css('visibility'), 'hidden');
-          assert.deepEqual(realOldElement.offset(), this.oldElement.offset(), "element didn't jump");
-          assert.equal(realOldElement.outerWidth(), this.oldElement.outerWidth(), "same width");
-          assert.equal(realOldElement.outerHeight(), this.oldElement.outerHeight(), "same height");
-          didTransition = true;
-          return Ember.RSVP.resolve();
-        }
-      })
-    );
-  });
-  this.render(stylesheet + `
-              {{#liquid-if showBlue class="explode-transition-test"}}
+            // the explode transition actually animates a copy of the
+            // original oldElement, which we can still find inside a
+            // liquid-child (the copy is not inside a liquid-child, that
+            // is part of the point of explode).
+            var realOldElement = this.oldElement.parent().find('.liquid-child .redbox');
+            assert.equal(realOldElement.length, 1, 'found actual old element');
+            assert.equal(realOldElement.css('visibility'), 'hidden');
+            assert.deepEqual(realOldElement.offset(), this.oldElement.offset(), "element didn't jump");
+            assert.equal(realOldElement.outerWidth(), this.oldElement.outerWidth(), "same width");
+            assert.equal(realOldElement.outerHeight(), this.oldElement.outerHeight(), "same height");
+            didTransition = true;
+            return Ember.RSVP.resolve();
+          }
+        })
+      );
+    });
+    this.render(stylesheet(boxSizing) + `
+                {{#liquid-if showBlue class="explode-transition-test"}}
                 <div class="bluebox"></div>
-              {{else}}
+                {{else}}
                 <div class="redbox"></div>
-              {{/liquid-if}}
-  `);
+                {{/liquid-if}}
+                `);
 
-  this.set('showBlue', true);
-  assert.ok(didTransition, 'didTransition');
-});
-
-
-
-test('it avoids a jump at start of animation, with statically positions elements', function(assert) {
-  var didTransition = false;
-  this.container.lookup('service:liquid-fire-transitions').map(function() {
-    this.transition(
-      this.hasClass('explode-transition-test'),
-      this.use('explode', {
-        pickOld: '.greenbox',
-        pickNew: '.yellowbox',
-        use: function() {
-          // sanity checks
-          assert.equal(this.oldElement && this.oldElement.length, 1, 'found old element');
-          assert.equal(this.newElement && this.newElement.length, 1, 'found new element');
-          assert.equal(this.oldElement && this.oldElement.css('background-color'), "rgb(0, 128, 0)");
-
-          // the explode transition actually animates a copy of the
-          // original oldElement, which we can still find inside a
-          // liquid-child (the copy is not inside a liquid-child, that
-          // is part of the point of explode).
-          var realOldElement = this.oldElement.parent().find('.liquid-child .greenbox');
-          assert.equal(realOldElement.length, 1, 'found actual old element');
-          assert.equal(realOldElement.css('visibility'), 'hidden');
-          assert.deepEqual(realOldElement.offset(), this.oldElement.offset(), "element didn't jump");
-          assert.equal(realOldElement.outerWidth(), this.oldElement.outerWidth(), "same width");
-          assert.equal(realOldElement.outerHeight(), this.oldElement.outerHeight(), "same height");
-          didTransition = true;
-          return Ember.RSVP.resolve();
-        }
-      })
-    );
+    this.set('showBlue', true);
+    assert.ok(didTransition, 'didTransition');
   });
-  this.render(stylesheet + `
-              {{#liquid-if showYellow class="explode-transition-test"}}
+
+
+
+  test(`it avoids a jump at start of animation, with statically positioned elements (${boxSizing})`, function(assert) {
+    var didTransition = false;
+    this.container.lookup('service:liquid-fire-transitions').map(function() {
+      this.transition(
+        this.hasClass('explode-transition-test'),
+        this.use('explode', {
+          pickOld: '.greenbox',
+          pickNew: '.yellowbox',
+          use: function() {
+            // sanity checks
+            assert.equal(this.oldElement && this.oldElement.length, 1, 'found old element');
+            assert.equal(this.newElement && this.newElement.length, 1, 'found new element');
+            assert.equal(this.oldElement && this.oldElement.css('background-color'), "rgb(0, 128, 0)");
+
+            // the explode transition actually animates a copy of the
+            // original oldElement, which we can still find inside a
+            // liquid-child (the copy is not inside a liquid-child, that
+            // is part of the point of explode).
+            var realOldElement = this.oldElement.parent().find('.liquid-child .greenbox');
+            assert.equal(realOldElement.length, 1, 'found actual old element');
+            assert.equal(realOldElement.css('visibility'), 'hidden');
+            assert.deepEqual(realOldElement.offset(), this.oldElement.offset(), "element didn't jump");
+            assert.equal(realOldElement.outerWidth(), this.oldElement.outerWidth(), "same width");
+            assert.equal(realOldElement.outerHeight(), this.oldElement.outerHeight(), "same height");
+            didTransition = true;
+            return Ember.RSVP.resolve();
+          }
+        })
+      );
+    });
+    this.render(stylesheet(boxSizing) + `
+                {{#liquid-if showYellow class="explode-transition-test"}}
                 <div class="yellowbox"></div>
-              {{else}}
+                {{else}}
                 <div class="greenbox"></div>
 
-              {{/liquid-if}}
-  `);
+                {{/liquid-if}}
+                `);
 
-  this.set('showYellow', true);
-  assert.ok(didTransition, 'didTransition');
+    this.set('showYellow', true);
+    assert.ok(didTransition, 'didTransition');
+  });
+
 });
 
-
-var stylesheet = `
+function stylesheet(boxSizing) {
+  return `
     <style>
     .explode-transition-test {
       width: 600px;
@@ -163,9 +166,9 @@ var stylesheet = `
       width: 20px;
       height: 25px;
       padding: 2px;
-      box-sizing: content-box;
       margin: 4px;
       border: 1px solid black;
+      box-sizing: ${boxSizing};
     }
     .redbox {
       background-color: red;
@@ -175,9 +178,9 @@ var stylesheet = `
       width: 25px;
       height: 30px;
       padding: 4px;
-      box-sizing: content-box;
       margin: 6px;
       border: 2px solid black;
+      box-sizing: ${boxSizing};
     }
     .yellowbox {
       background-color: yellow;
@@ -186,8 +189,8 @@ var stylesheet = `
       width: 20px;
       height: 25px;
       padding: 2px;
-      box-sizing: content-box;
       border: 1px solid black;
+      box-sizing: ${boxSizing};
     }
     .greenbox {
       background-color: green;
@@ -196,9 +199,10 @@ var stylesheet = `
       width: 25px;
       height: 30px;
       padding: 4px;
-      box-sizing: content-box;
       border: 2px solid black;
+      box-sizing: ${boxSizing};
     }
               </style>
 
-`;
+    `;
+}
