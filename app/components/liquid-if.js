b130e5f1bd2aaf6848b3ddc81f3dce1e37420e5e
Edward Faulkner <ef@alum.mit.edu>
2015-05-06 02:38:41 -0400

    updating liquid-if

--- app/components/liquid-if.js
+++ app/components/liquid-if.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 import { inverseYieldMethod } from "liquid-fire/ember-internals";
 
 export default Ember.Component.extend({
+  positionalParams: ['value'],
+  tagName: '',
   _yieldInverse: inverseYieldMethod,
   hasInverse: Ember.computed('inverseTemplate', function() {
     return !!this.get('inverseTemplate');
