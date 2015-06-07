5a3dd92e17552124de54c94d4763b018c11a5c13
Edward Faulkner <ef@alum.mit.edu>
2015-06-07 16:15:23 -0700

    work-in-progress to replace liquid-with with a liquid-bind block form

--- tests/integration/helpers/liquid-bind-block-form-test.js
+++ tests/integration/helpers/liquid-bind-block-form-test.js
@@ -5,7 +5,7 @@ import { test } from "ember-qunit";
 import { setOutletState, withTemplate } from "../../helpers/outlet";
 import QUnit from "qunit";
 
-moduleForIntegration('Integration: liquid-with', {
+moduleForIntegration('Integration: liquid-bind block form', {
   teardown: function() {
     QUnit.stop();
     var tmap = this.container.lookup('service:liquid-fire-transitions');
@@ -16,19 +16,19 @@ moduleForIntegration('Integration: liquid-with', {
 test('it should render', function(assert) {
   this.set('title', 'Mr');
   this.set('person', 'Tom');
-  this.render("{{#liquid-with person as |p|}}{{title}}:{{p}}{{/liquid-with}}");
+  this.render("{{#liquid-bind person as |p|}}{{title}}:{{p}}{{/liquid-bind}}");
   assert.equal(this.$().text().trim(), 'Mr:Tom');
 });
 
 test('it should update', function(assert) {
   this.set('person', 'Tom');
-  this.render("{{#liquid-with person as |p|}}A{{p}}B{{/liquid-with}}");
+  this.render("{{#liquid-bind person as |p|}}A{{p}}B{{/liquid-bind}}");
   this.set('person', 'Yehua');
   assert.equal(this.$().text().trim(), 'AYehuaB');
 });
 
 test('it should support element id', function(assert) {
-  this.render('{{#liquid-with foo id="foo" as |bar|}} {{/liquid-with}}');
+  this.render('{{#liquid-bind foo id="foo" as |bar|}} {{/liquid-bind}}');
   assert.equal(this.$('.liquid-container#foo').length, 1, "found element by id");
 });
 
@@ -37,12 +37,12 @@ test('it should animate after initially rendering empty', function(assert) {
   var dummyAnimation = function(){ return Ember.RSVP.resolve(); };
   tmap.map(function() {
     this.transition(
-      this.inHelper('liquid-with'),
+      this.inHelper('liquid-bind'),
       this.use(dummyAnimation)
     );
   });
   sinon.spy(tmap, 'transitionFor');
-  this.render('{{#liquid-with foo as |bar|}} {{/liquid-with}}');
+  this.render('{{#liquid-bind foo as |bar|}} {{/liquid-bind}}');
   assert.equal(this.$('.liquid-child').length, 0, "initially no child");
   assert.ok(tmap.transitionFor.notCalled, "did not animate");
   this.set('foo', 'hi');
@@ -53,20 +53,20 @@ test('it should animate after initially rendering empty', function(assert) {
 
 test('should support containerless mode', function(assert) {
   this.set('foo', 'Hi');
-  this.render('{{#liquid-with foo containerless=true as |bar| }}{{foo}}{{/liquid-with}}');
+  this.render('{{#liquid-bind foo containerless=true as |bar| }}{{foo}}{{/liquid-bind}}');
   assert.equal(this.$('.liquid-container').length, 0, "no container");
   assert.equal(this.$(' > .liquid-child').length, 1, "direct liquid child");
 });
 
 test('should support `class` in containerless mode', function(assert) {
   this.set('foo', 'Hi');
-  this.render('{{#liquid-with foo class="bar" containerless=true as |bar| }}{{foo}}{{/liquid-with}}');
+  this.render('{{#liquid-bind foo class="bar" containerless=true as |bar| }}{{foo}}{{/liquid-bind}}');
   assert.equal(this.$(' > .liquid-child.bar').length, 1, "direct liquid child");
 });
 
 test('should pass container arguments through', function(assert) {
   this.set('foo', 'Hi');
-  this.render('{{#liquid-with foo enableGrowth=false as |bar|}}{{foo}}{{/liquid-with}}');
+  this.render('{{#liquid-bind foo enableGrowth=false as |bar|}}{{foo}}{{/liquid-bind}}');
   var containerElement = this.$(' > .liquid-container');
   var container = Ember.View.views[containerElement.attr('id')];
   assert.equal(container.get('enableGrowth'), false, 'liquid-container enableGrowth');
