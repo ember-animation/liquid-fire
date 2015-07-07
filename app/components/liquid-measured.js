bd64d03fe1f22ee2cf840eb116a7932ccb37c3c5
Doug DeBold <ddebold56@gmail.com>
2015-07-06 23:17:39 -0500

    Added comments in import/export statements

--- app/components/liquid-measured.js
+++ app/components/liquid-measured.js
@@ -1,4 +1,4 @@
 import LiquidMeasured, { measure } from 'liquid-fire/components/liquid-measured';
-
+// Using import ... export instead of export default ... to be compatible with IE8
 export default LiquidMeasured;
 export { measure };
--- app/components/liquid-spacer.js
+++ app/components/liquid-spacer.js
@@ -1,3 +1,3 @@
 import LiquidSpacer from "liquid-fire/components/liquid-spacer";
-
+// Using import ... export instead of export default ... to be compatible with IE8
 export default LiquidSpacer;
