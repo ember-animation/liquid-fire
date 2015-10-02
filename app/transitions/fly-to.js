4b22a58613ff119d495f0e18de4257072985ebe8
Edward Faulkner <ef@alum.mit.edu>
2015-10-02 01:56:51 -0400

    pure transform-based fly-to

--- app/transitions/fly-to.js
+++ app/transitions/fly-to.js
@@ -1,4 +1,5 @@
 import { animate, Promise } from "liquid-fire";
+import { currentTransform } from 'liquid-fire/matrix';
 
 export default function flyTo(opts={}) {
   if (!this.newElement) {
@@ -8,27 +9,38 @@ export default function flyTo(opts={}) {
     return Promise.resolve();
   }
 
-  var oldOffset = this.oldElement.offset();
-  var newOffset = this.newElement.offset();
+  var oldRect = this.oldElement[0].getBoundingClientRect();
+  var newRect = this.newElement[0].getBoundingClientRect();
 
+  var relativeTranslateX = newRect.left - oldRect.left;
+  var relativeTranslateY = newRect.top - oldRect.top;
+  var relativeScaleX = (newRect.right - newRect.left) / (oldRect.right - oldRect.left);
+  var relativeScaleY = (newRect.bottom - newRect.top) / ( oldRect.bottom - oldRect.top);
 
   if (opts.movingSide === 'new') {
+    let initialTransform = currentTransform(this.newElement);
     let motion = {
-      translateX: [0, oldOffset.left - newOffset.left],
-      translateY: [0, oldOffset.top - newOffset.top],
-      outerWidth: [this.newElement.outerWidth(), this.oldElement.outerWidth()],
-      outerHeight: [this.newElement.outerHeight(), this.oldElement.outerHeight()]
+      translateX: [initialTransform.tx, initialTransform.tx - relativeTranslateX],
+      translateY: [initialTransform.ty, initialTransform.ty - relativeTranslateY],
+      scaleX: [ initialTransform.a, initialTransform.a / relativeScaleX],
+      scaleY: [ initialTransform.d, initialTransform.d / relativeScaleY]
     };
     this.oldElement.css({ visibility: 'hidden' });
+    // the currentTransform function above consumes transform-origin
+    // and accounts for it in the initialTransform. So we zero-out the
+    // actual transform-origin and all is well.
+    this.newElement.css('transform-origin', '0 0');
     return animate(this.newElement, motion, opts);
   } else {
+    let initialTransform = currentTransform(this.oldElement);
     let motion = {
-      translateX: newOffset.left - oldOffset.left,
-      translateY: newOffset.top - oldOffset.top,
-      outerWidth: this.newElement.outerWidth(),
-      outerHeight: this.newElement.outerHeight()
+      translateX: [relativeTranslateX + initialTransform.tx, initialTransform.tx],
+      translateY: [relativeTranslateY + initialTransform.ty, initialTransform.ty],
+      scaleX: [ initialTransform.a * relativeScaleX, initialTransform.a ],
+      scaleY: [ initialTransform.d * relativeScaleY, initialTransform.d ]
     };
     this.newElement.css({ visibility: 'hidden' });
+    this.oldElement.css('transform-origin', '0 0');
     return animate(this.oldElement, motion, opts).then(() => {
       this.newElement.css({ visibility: ''});
     });
