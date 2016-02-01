e2160cd975bbe972c9b4e47e8ab11862c8a75cbb
Ilya Radchenko <ilya@burstcreations.com>
2016-02-01 11:12:17 -0500

    Document `outletName` constraint
    The long needed docs for https://github.com/ember-animation/liquid-fire/pull/341

---
+++ tests/dummy/snippets/outlet-match-examples.js
@@ -0,0 +1,21 @@
+// Matches any transition that ends up in the outlet named 'panel', no
+// matter where it came from.
+// {{liquid-outlet 'panel'}}
+this.transition(
+  this.outletName('panel'),
+  this.use('toLeft')
+);
+
+// Matches nothing to something in the 'panel' outlet
+this.transition(
+  this.outletName('panel'),
+  this.fromRoute(null),
+  this.use('toLeft', { duration: 100, easing: 'easeInOut' }),
+  this.reverse('toRight', { duration: 500, easing: 'easeInOut' })
+);
+
+// Matches the default outlet only
+this.transition(
+  this.outletName('main'),
+  this.use('crossFade')
+);
