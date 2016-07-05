d2b704ade2cee8b4ae3e9ede82237b1f151875db
Edward Faulkner <ef@alum.mit.edu>
2016-07-05 15:24:19 -0400

    moving implementations of liquid-unless

---
+++ addon/components/liquid-unless.js
@@ -0,0 +1,6 @@
+import LiquidIf from './liquid-if';
+
+export default LiquidIf.extend({
+  helperName: 'liquid-unless',
+  inverted: true
+});
--- app/components/liquid-unless.js
+++ app/components/liquid-unless.js
@@ -1,7 +1,1 @@
-import LiquidIf from './liquid-if';
-
-export default LiquidIf.extend({
-  helperName: 'liquid-unless',
-  layoutName: 'components/liquid-if',
-  inverted: true
-});
+export { default } from "liquid-fire/components/liquid-unless";
