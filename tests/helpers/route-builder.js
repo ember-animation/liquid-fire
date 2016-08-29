da09c665b88352dbe494f2b26ec238217ba054a7
Edward Faulkner <ef@alum.mit.edu>
2016-08-28 20:57:48 -0400

    refactor liquid-outlet tests

---
+++ tests/helpers/route-builder.js
@@ -0,0 +1,47 @@
+import Ember from 'ember';
+
+class RouteInfo {
+  constructor(builder, { template, controller }, outlets = {}) {
+    this.builder = builder;
+    this.outlets = outlets;
+    this.render = {
+      template,
+      controller
+    };
+  }
+  setChild(name, args) {
+    return (this.outlets[name] = this.builder.makeRoute(args));
+  }
+  asTop() {
+    return {
+      outlets: {
+        main: this._serialize()
+      }
+    };
+  }
+  _serialize() {
+    let outlets = {};
+    Object.keys(this.outlets).forEach(key => {
+      outlets[key] = this.outlets[key]._serialize();
+    });
+    return {
+      render: this.render,
+      outlets
+    };
+  }
+}
+
+export default Ember.Service.extend({
+  makeRoute(args) {
+    if (args.template) {
+      args.template = this._prepareTemplate(args.template);
+    }
+    return new RouteInfo(this, args);
+  },
+  _prepareTemplate(compiled) {
+    let name = `template:${Ember.guidFor({})}`;
+    let owner = Ember.getOwner(this);
+    owner.register(name, compiled);
+    return owner.lookup(name);
+  }
+});
--- tests/integration/helpers/liquid-outlet-test.js
+++ tests/integration/helpers/liquid-outlet-test.js
@@ -2,54 +2,26 @@
 import Ember from "ember";
 import { skip } from 'qunit';
 import { test, moduleForComponent } from "ember-qunit";
-import { withTemplate } from "../../helpers/outlet";
-
-var top, topState, controller, stageView;
-
-function setOutletState(state) {
-  Ember.run(() => {
-    topState.outlets.main = state;
-    top.setOutletState(topState);
-  });
-}
+import hbs from 'htmlbars-inline-precompile';
+import RouteBuilder from '../../helpers/route-builder';
 
 moduleForComponent('Integration: liquid-outlet', {
   integration: true,
   beforeEach() {
-    controller = Ember.Controller.create();
-    topState = {
-      render: {},
-      outlets: {}
+    this.register('service:route-builder', RouteBuilder);
+    this.inject.service('route-builder', { as: 'builder' });
+    this.register('component:set-route', Ember.Component.extend({
+      tagName: '',
+      layout: hbs`{{#-with-dynamic-var "outletState" state}}{{yield}}{{/-with-dynamic-var}}`
+    }));
+    this.setState = function(routeInfo) {
+      this.set('state', routeInfo.asTop());
     };
-    top = this.container.lookup('view:-outlet');
-    top.setOutletState(topState);
-
-    this.render = function(template) {
-      topState = withTemplate(template);
-      topState.render.controller = controller;
-      topState.render.ViewClass = Ember.Component.extend({
-        didInsertElement() {
-          stageView = this;
-        }
-      });
-      Ember.run(() => top.setOutletState(topState));
+    this.makeRoute = function(args) {
+      return this.get('builder').makeRoute(args);
     };
-
-    this.$ = function() {
-      return stageView.$.apply(stageView, arguments);
-    };
-
-    this.set = function(k,v) {
-      controller.set(k,v);
-    };
-
-    Ember.run(() => top.appendTo('#ember-testing'));
   },
   afterEach(assert) {
-    Ember.run(() => {
-      top.destroy();
-      controller.destroy();
-    });
     let done = assert.async();
     var tmap = this.container.lookup('service:liquid-fire-transitions');
     tmap.waitUntilIdle().then(done);
@@ -57,30 +29,28 @@ moduleForComponent('Integration: liquid-outlet', {
 });
 
 test('it should render when state is set after insertion', function(assert) {
-  this.render('{{liquid-outlet}}');
-  setOutletState(withTemplate('<h1>Hello world</h1>'));
+  this.render(hbs`{{#set-route state=state}}{{liquid-outlet}}{{/set-route}}`);
+  this.setState(this.makeRoute({ template: hbs`<h1>Hello world</h1>`}));
   assert.equal(this.$('h1').length, 1);
 });
 
 test('it should render when state is set before insertion', function(assert) {
-  var routerState;
-  this.render('A{{outlet}}B');
-  routerState = withTemplate('Hello{{liquid-outlet}}');
-  setOutletState(routerState);
+  this.render(hbs`{{#set-route state=state}}A{{outlet}}B{{/set-route}}`);
+  let hello = this.makeRoute({ template: hbs`Hello{{liquid-outlet}}` });
+  this.setState(hello);
   assert.equal(this.$().text().trim(), 'AHelloB');
-  routerState.outlets.main = withTemplate('Goodbye');
-  setOutletState(routerState);
+  hello.setChild('main', { template: hbs`Goodbye` });
+  this.setState(hello);
   assert.equal(this.$().text().trim(), 'AHelloGoodbyeB');
 });
 
 test('it should support an optional name', function(assert) {
-  var routerState;
-  this.render('A{{outlet}}B');
-  routerState = withTemplate('Hello{{liquid-outlet "other"}}');
-  setOutletState(routerState);
+  this.render(hbs`{{#set-route state=state}}A{{outlet}}B{{/set-route}}`);
+  let hello = this.makeRoute({ template: hbs`Hello{{liquid-outlet "other"}}` });
+  this.setState(hello);
   assert.equal(this.$().text().trim(), 'AHelloB');
-  routerState.outlets.other = withTemplate('Goodbye');
-  setOutletState(routerState);
+  hello.setChild('other', { template: hbs`Goodbye` });
+  this.setState(hello);
   assert.equal(this.$().text().trim(), 'AHelloGoodbyeB');
 });
 
@@ -111,93 +81,77 @@ skip('should pass container arguments through', function(assert) {
 });
 
 test('it should support `use` option', function(assert) {
-  var tmap = this.container.lookup('service:liquid-fire-transitions');
+  var tmap = Ember.getOwner(this).lookup('service:liquid-fire-transitions');
   sinon.spy(tmap, 'transitionFor');
-  this.render('{{outlet}}');
-  var routerState = withTemplate('{{liquid-outlet use="fade"}}');
-  routerState.outlets.main = withTemplate('hi');
-  setOutletState(routerState);
-  routerState.outlets.main = withTemplate('byte');
-  setOutletState(routerState);
+  this.render('{{#set-route state=state}}{{outlet}}{{/set-route}}');
+  var routerState = this.makeRoute({ template: hbs`{{liquid-outlet use="fade"}}` });
+  routerState.setChild('main', { template: hbs`hi` });
+  this.setState(routerState);
+  routerState.setChild('main', { template: hbs`byte` });
+  this.setState(routerState);
+  assert.ok(tmap.transitionFor.called, 'transitionFor should be called');
   assert.equal(tmap.transitionFor.lastCall.returnValue.animation.name, 'fade');
   //return tmap.waitUntilIdle();
 });
 
 test('should support containerless mode', function(assert) {
-  this.render('{{liquid-outlet containerless=true}}');
-  setOutletState(withTemplate('<h1>Hello world</h1>'));
+  this.render(hbs`{{#set-route state=state}}{{liquid-outlet containerless=true}}{{/set-route}}`);
+  this.setState(this.makeRoute({ template: hbs`<h1>Hello world</h1>` }));
   assert.equal(this.$('.liquid-container').length, 0, "no container");
   assert.equal(this.$(' > .liquid-child').length, 1, "direct liquid child");
 });
 
 test('should support `class` on children in containerless mode', function(assert) {
-  this.render('{{liquid-outlet class="bar" containerless=true}}');
-  setOutletState(withTemplate('<h1>Hello world</h1>'));
+  this.render(hbs`{{#set-route state=state}}{{liquid-outlet class="bar" containerless=true}}{{/set-route}}`);
+  this.setState(this.makeRoute({ template: hbs`<h1>Hello world</h1>` }));
   assert.equal(this.$(' > .liquid-child.bar').length, 1, "child class");
 });
 
 test('can see model-to-model transitions on the same route', function(assert) {
-  var state = {
-    render: {
-      template: Ember.Handlebars.compile('<div class="content">{{model.id}}</div>'),
-      controller: Ember.Controller.create({
-        model: Ember.Object.create({
-          id: 1
-        })
-      })
-    },
-    outlets: {}
-  };
-  var tmap = this.container.lookup('service:liquid-fire-transitions');
+  let controller = Ember.getOwner(this).lookup('controller:application');
+  controller.set('model', Ember.Object.create({
+    id: 1
+  }));
+  let state = this.makeRoute({
+    template: hbs`'<div class="content">{{model.id}}</div>`,
+    controller
+  });
+  var tmap = Ember.getOwner(this).lookup('service:liquid-fire-transitions');
   sinon.spy(tmap, 'transitionFor');
-  this.render('{{liquid-outlet watchModels=true}}');
-  setOutletState(state);
+  this.render(hbs`{{#set-route state=state}}{{liquid-outlet watchModels=true}}{{/set-route}}`);
+  this.setState(state);
   assert.equal(this.$('.content').text().trim(), '1');
   tmap.transitionFor.reset();
   Ember.run(() => {
-    state.render.controller.set('model', Ember.Object.create({
+    controller.set('model', Ember.Object.create({
       id: 2
     }));
   });
-  setOutletState(state);
+  this.setState(state);
   assert.equal(this.$('.content').text().trim(), '2');
   assert.ok(tmap.transitionFor.called, 'transitionFor called');
 });
 
 test('tolerates empty content when parent outlet is stable', function(assert) {
-  this.render('A{{liquid-outlet}}B');
+  this.render(hbs`{{#set-route state=state}}A{{liquid-outlet}}B{{/set-route}}`);
 
-  var state = {
-    render: {
-      template: Ember.Handlebars.compile('C{{liquid-outlet "a"}}D{{liquid-outlet "b"}}E')
-    },
-    outlets: {}
-  };
+  let state = this.makeRoute({
+    template: hbs`C{{liquid-outlet "a"}}D{{liquid-outlet "b"}}E`
+  });
 
-  setOutletState(state);
+  this.setState(state);
   assert.equal(this.$().text().trim(), 'ACDEB');
-  state.outlets.a = {
-    render: { template: Ember.Handlebars.compile('foo') },
-    outlets: {}
-  };
-  setOutletState(state);
+  state.setChild('a', { template: hbs`foo` });
+  this.setState(state);
   assert.equal(this.$().text().trim(), 'ACfooDEB');
 });
 
 test('outlets inside {{liquid-bind}}', function(assert) {
-
-  var routerState = withTemplate("{{#liquid-bind thing as |thingVersion|}}{{thingVersion}}{{outlet}}{{/liquid-bind}}");
-  routerState.outlets.main = withTemplate("Hello");
-  routerState.render.controller = Ember.Object.create({
-    thing: 'Goodbye'
-  });
-  this.render('{{outlet}}');
-  setOutletState(routerState);
+  this.set('thing', 'Goodbye');
+  this.setState(this.makeRoute({ template: hbs`Hello` }));
+  this.render(hbs`{{#set-route state=state}}{{#liquid-bind thing as |thingVersion|}}{{thingVersion}}{{outlet}}{{/liquid-bind}}{{/set-route}}`);
   assert.equal(this.$().text().trim(), 'GoodbyeHello');
-  Ember.run(() => {
-    routerState.render.controller.set('thing', 'Other');
-  });
-  routerState.outlets.main = withTemplate("Purple");
-  setOutletState(routerState);
+  this.set('thing', 'Other');
+  this.setState(this.makeRoute({ template: hbs`Purple` }));
   assert.equal(this.$().text().trim(), 'OtherPurple');
 });
