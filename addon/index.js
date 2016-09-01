a622248a87f2281efbb77d7fef830dc7f5acdf73
Edward Faulkner <ef@alum.mit.edu>
2016-09-01 12:56:51 -0400

    moving remaining version checks to build time

--- addon/index.js
+++ addon/index.js
@@ -2,14 +2,8 @@ import TransitionMap from "./transition-map";
 import { animate, stop, isAnimating, timeSpent, timeRemaining, finish } from "./animate";
 import Promise from "./promise";
 import MutationObserver from "./mutation-observer";
-import versionWarnings from "./version-warnings";
 import "./velocity-ext";
 export { default as Pausable } from './mixins/pausable';
-
-versionWarnings({
-  minVelocityVersion: [0, 11, 8]
-});
-
 
 export { TransitionMap, animate, stop, isAnimating, timeSpent, timeRemaining, finish,
          Promise, MutationObserver };
--- index.js
+++ index.js
@@ -16,6 +16,7 @@ module.exports = {
 
     this.versionChecker = new VersionChecker(this);
     this.versionChecker.for('ember-cli', 'npm').assertAbove('0.2.0');
+    this.versionChecker.for('velocity-animate', 'npm').assertAbove('0.11.8');
   },
 
 
