055fe5978e8337da8ccf25a097bd86807f5c36b9
Robert Jackson <rwjblue@twitch.tv>
2016-09-07 22:11:40 -0400

    Fix issues with glimmer2 branch.
    * Avoid using `let` in Node-land (grrrr).
    * Avoid using concise functions in Node-land.
    * Fix issue when `liquid-fire` is not the current working directory
      while building (needed to `path.join` the addon's root before
      funneling).
    * Use `Ember.ViewUtils.getViewBounds` instead of `Ember.__loader.require`.

--- version-specific-2.9/index.js
+++ version-specific-2.9/index.js
@@ -1,10 +1,8 @@
 import Ember from 'ember';
-let getViewBounds;
 
-export function initialize() {
-  let emberRequire = Ember.__loader.require;
-  getViewBounds = emberRequire('ember-views/system/utils').getViewBounds;
-}
+const { getViewBounds } = Ember.ViewUtils;
+
+export function initialize() { }
 
 export function containingElement(view) {
   return getViewBounds(view).parentElement;
