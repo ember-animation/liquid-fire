2c7c1e29b16507eb9a7f723a4765264fec0294de
Edward Faulkner <ef@alum.mit.edu>
2016-09-16 21:24:16 -0400

    make default outletName work in ember < 2.3

--- addon/components/liquid-outlet.js
+++ addon/components/liquid-outlet.js
@@ -8,7 +8,6 @@ import {
 
 var LiquidOutlet = Ember.Component.extend({
   layout,
-  outletName: 'main',
   positionalParams: ['outletName'], // needed for Ember 1.13.[0-5] and 2.0.0-beta.[1-3] support
   tagName: '',
   versionEquality: Ember.computed('outletName', 'watchModels', function() {
@@ -19,7 +18,12 @@ var LiquidOutlet = Ember.Component.extend({
       let newChild = childRoute(newValue, outletName);
       return routeIsStable(oldChild, newChild) && (!watchModels || modelIsStable(oldChild, newChild));
     };
-  })
+  }),
+  didReceiveAttrs() {
+    if (!this.get('outletName')) {
+      this.set('outletName', 'main');
+    }
+  }
 });
 
 LiquidOutlet.reopenClass({
