2b966b16df3cd9c8be3724afe015a3ea5da0a371
Edward Faulkner <ef@alum.mit.edu>
2016-09-16 22:03:32 -0400

    fix default positionalParam handling across ember versions

--- addon/components/liquid-outlet.js
+++ addon/components/liquid-outlet.js
@@ -8,7 +8,7 @@ import {
 
 var LiquidOutlet = Ember.Component.extend({
   layout,
-  positionalParams: ['outletName'], // needed for Ember 1.13.[0-5] and 2.0.0-beta.[1-3] support
+  positionalParams: ['inputOutletName'], // needed for Ember 1.13.[0-5] and 2.0.0-beta.[1-3] support
   tagName: '',
   versionEquality: Ember.computed('outletName', 'watchModels', function() {
     let outletName = this.get('outletName');
@@ -20,14 +20,13 @@ var LiquidOutlet = Ember.Component.extend({
     };
   }),
   didReceiveAttrs() {
-    if (!this.get('outletName')) {
-      this.set('outletName', 'main');
-    }
+    this._super();
+    this.set('outletName', this.get('inputOutletName') || 'main');
   }
 });
 
 LiquidOutlet.reopenClass({
-  positionalParams: ['outletName']
+  positionalParams: ['inputOutletName']
 });
 
 export default LiquidOutlet;
