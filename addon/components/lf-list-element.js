444354dafe329376b1f8099422d88e6fb71f78f5
Edward Faulkner <ef@alum.mit.edu>
2016-10-23 16:18:14 -0700

    progress

--- addon/components/lf-list-element.js
+++ addon/components/lf-list-element.js
@@ -3,6 +3,7 @@ import layout from '../templates/components/lf-list-element';
 import { componentNodes } from 'liquid-fire/ember-internals';
 import $ from 'jquery';
 import velocity from 'velocity';
+import RSVP from 'rsvp';
 
 export default Ember.Component.extend({
   layout,
@@ -14,7 +15,11 @@ export default Ember.Component.extend({
     this.sendAction("entering", this);
   },
   willDestroyElement() {
-    this.sendAction("leaving", this);
+    let copies = [];
+    this._forEachElement(elt => {
+      copies.push($(elt).clone());
+    });
+    this.sendAction("leaving", this, copies);
   },
 
   _forEachElement(fn) {
@@ -22,7 +27,7 @@ export default Ember.Component.extend({
     let node = firstNode;
     while (node) {
       if (node.nodeType === Node.ELEMENT_NODE) {
-        fn($(node));
+        fn(node);
       } else if (! /^\s*$/.test(node.textContent)) {
         console.warn("Found bare text content inside a liquid-each");
       }
@@ -32,12 +37,70 @@ export default Ember.Component.extend({
   },
 
   reveal() {
+    let promises = [];
     this._forEachElement(elt => {
-      velocity(elt, { opacity: [1, 0]}, { visibility: 'visible', duration: 500 })
+      promises.push(velocity(elt, { opacity: [1, 0]}, { visibility: 'visible', duration: 500 })
         .then(() => $(elt).css({
           visibility: '',
           opacity: ''
-        }));
+        })));
     });
+    return RSVP.all(promises);
+  },
+
+
+  measure() {
+    let measurements = [];
+    this._forEachElement(elt => {
+      let bounds = elt.getBoundingClientRect();
+      let parentBounds = elt.offsetParent.getBoundingClientRect();
+      measurements.push({
+        elt,
+        width: bounds.width,
+        height: bounds.height,
+        x: bounds.left - parentBounds.left,
+        y: bounds.top - parentBounds.top
+      });
+    });
+    return measurements;
+  },
+
+  lock(measurements) {
+    measurements.forEach(({elt, width, height, x, y}) => {
+      $(elt).css({
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width,
+        height,
+        transform: `translateX(${x}px) translateY(${y}px)`
+      });
+    });
+  },
+
+  unlock() {
+    this._forEachElement(elt => {
+      $(elt).css({
+        position: '',
+        top: '',
+        left: '',
+        width: '',
+        height: '',
+        transform: ''
+      });
+    });
+  },
+
+  move(oldMeasurements, newMeasurements) {
+    let promises = [];
+    this._forEachElement(elt => {
+      let oldMeasurement = oldMeasurements.find(m => m.elt === elt);
+      let newMeasurement = newMeasurements.find(m => m.elt === elt);
+      return velocity(elt, {
+        translateX: [newMeasurement.x, oldMeasurement.x],
+        translateY: [newMeasurement.y, oldMeasurement.y]
+      }, { duration: 500 });
+    });
+    return RSVP.all(promises);
   }
 });
--- addon/components/liquid-each.js
+++ addon/components/liquid-each.js
@@ -1,23 +1,43 @@
 import Ember from 'ember';
 import layout from '../templates/components/liquid-each';
+import RSVP from 'rsvp';
 
 export default Ember.Component.extend({
   layout,
   tagName: '',
   init() {
     this._super();
-    this._entering = null;
-    this._leaving = null;
+    this._entering = [];
+    this._current = [];
+    this._leaving = [];
   },
   didReceiveAttrs() {
-    this._entering = [];
-    this._leaving = [];
+    let positions = this._current.map(component => component.measure());
+    this._current.forEach((component, index) => component.lock(positions[index]));
+
     Ember.run.schedule('afterRender', () => {
-      console.log(`${this._entering.length} new children, ${this._leaving.length} removed children`);
-      this._entering.forEach(component => {
-        component.reveal();
+      // Briefly unlock everybody, so we can measure the final static layout
+      this._current.forEach(component => component.unlock());
+      let movedPositions = this._current.map(component => component.measure());
+      let insertedPositions = this._entering.map(component => component.measure());
+      this._current.forEach((component, index) => component.lock(positions[index]));
+      this._entering.forEach((component, index) => component.lock(insertedPositions[index]));
+
+      let promises = this._entering.map(component => component.reveal()).concat(
+        this._current.map((component, index) => component.move(positions[index], movedPositions[index])));
+
+      RSVP.all(promises).then(() => {
+        this._current.forEach(component => component.unlock());
+        this._entering.forEach(component => component.unlock());
+        this.finalizeAnimation();
       });
     });
+  },
+
+  finalizeAnimation() {
+    this._current = this._current.filter(component => this._leaving.indexOf(component) === -1).concat(this._entering);
+    this._entering = [];
+    this._leaving = [];
   },
 
   actions: {
