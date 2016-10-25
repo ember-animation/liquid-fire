cbcd82e18e7a3020fb8ce6934bb07181f1dfe865
Edward Faulkner <ef@alum.mit.edu>
2016-10-25 07:01:23 -0400

    glimmer2 too

--- version-specific-2.9/index.js
+++ version-specific-2.9/index.js
@@ -8,4 +8,12 @@ export function containingElement(view) {
   return getViewBounds(view).parentElement;
 }
 
+export function componentNodes(view) {
+  let bounds = getViewBounds(view);
+  return {
+    firstNode: bounds.firstNode,
+    lastNode: bounds.lastNode
+  };
+}
+
 export { default as getOutletStateTemplate } from 'liquid-fire/templates/version-specific/get-outlet-state';
