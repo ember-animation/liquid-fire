c74ddb7b6934a2a9cca8c7f64361fd6bb19cb479
Edward Faulkner <ef@alum.mit.edu>
2016-10-30 22:34:47 +0100

    starting animated-container tests
    these don't fully pass yet, pushing this onto the stack to work on Measurement tests first

--- tests/integration/components/animated-container-test.js
+++ tests/integration/components/animated-container-test.js
@@ -1,24 +1,76 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
 moduleForComponent('animated-container', 'Integration | Component | animated container', {
-  integration: true
+  integration: true,
+  beforeEach() {
+    let here = this;
+    this.register('component:grab-container', Ember.Component.extend({
+      didReceiveAttrs() {
+        here.set('grabbed', this.get('cont'));
+      }
+    }));
+  }
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{animated-container}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
+test('simple render', function(assert) {
   this.render(hbs`
-    {{#animated-container}}
-      template block text
+    {{#animated-container as |container|}}
+      <div class="inside">
+        {{grab-container cont=container}}
+      </div>
     {{/animated-container}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  this.$('.inside').css({
+    height: 210
+  });
+
+  let container = bounds(this.$('.animated-container'));
+  let inside = bounds(this.$('.inside'));
+  assert.equal(container.height, inside.height, 'takes height of content');
+
+  this.$('.inside').css({
+    height: 600
+  });
+
+  container = bounds(this.$('.animated-container'));
+  let tallerInside = bounds(this.$('.inside'));
+  assert.equal(container.height, tallerInside.height, 'adapts to height of content');
+  assert.ok(tallerInside.height > inside.height, "inside content got taller");
+
 });
+
+test('locks size', function(assert) {
+  this.render(hbs`
+    {{#animated-container as |container|}}
+      <div class="inside">
+        {{grab-container cont=container}}
+      </div>
+    {{/animated-container}}
+  `);
+
+  this.$('.inside').css({
+    height: 210
+  });
+
+  let original = bounds(this.$('.animated-container'));
+
+  this.get('grabbed.lock')();
+
+  this.$('.inside').css({
+    height: 600
+  });
+
+  let final = bounds(this.$('.animated-container'));
+
+  assert.equal(final.height, original.height, 'height can be locked');
+});
+
+function bounds($elt) {
+  return {
+    width: $elt.outerWidth(),
+    height: $elt.outerHeight()
+  };
+}
