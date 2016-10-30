e53a1856e1bf981191c6c264ce51d1b80936899f
Edward Faulkner <ef@alum.mit.edu>
2016-10-30 15:46:54 +0100

    basic test for animated-each

---
+++ tests/integration/components/animated-each-test.js
@@ -0,0 +1,30 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import QUnit from 'qunit';
+import $ from 'jquery';
+
+moduleForComponent('liquid-each', 'Integration | Component | animated each', {
+  integration: true,
+  beforeEach(assert) {
+    assert.listContents = function( $elts, expected, message ) {
+      let values = $elts.toArray().map(e => $(e).text().trim());
+      this.pushResult({
+        result: QUnit.equiv(values, expected),
+        actual: values,
+        expected: expected,
+        message: message
+      });
+    };
+  }
+});
+
+test('it renders', function(assert) {
+  this.set('items', ['a', 'b', 'c']);
+  this.render(hbs`
+    {{#animated-each items as |item|}}
+      <div class="test-child">{{item}}</div>
+    {{/animated-each}}
+  `);
+
+  assert.listContents(this.$('.test-child'), ['a', 'b', 'c']);
+});
