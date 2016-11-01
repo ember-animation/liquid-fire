500ac56520d863890d936cb1024796a849f3c950
Edward Faulkner <ef@alum.mit.edu>
2016-11-01 14:36:33 -0400

    switching to a more transform-agnotic locking

--- addon/measurement.js
+++ addon/measurement.js
@@ -3,24 +3,22 @@ import Move from 'liquid-fire/motions/move';
 
 export default class Measurement {
   constructor(elt) {
-    let bounds = elt.getBoundingClientRect();
-    let parentBounds = elt.offsetParent.getBoundingClientRect();
     this.elt = elt;
     this.parentElement = elt.parentElement;
-    this.width = bounds.width;
-    this.height = bounds.height;
-    this.x = bounds.left - parentBounds.left;
-    this.y = bounds.top - parentBounds.top;
-    this._styleCache = $(this.elt).attr('style') || null;
+    this.width = elt.offsetWidth;
+    this.height = elt.offsetHeight;
+    this.x = elt.offsetLeft;
+    this.y = elt.offsetTop;
+    this._styleCache = $(elt).attr('style') || null;
   }
   lock() {
     $(this.elt).css({
       position: 'absolute',
-      top: 0,
-      left: 0,
+      boxSizing: 'border-box',
+      top: this.y,
+      left: this.x,
       width: this.width,
-      height: this.height,
-      transform: `translateX(${this.x}px) translateY(${this.y}px)`
+      height: this.height
     });
   }
   unlock() {
