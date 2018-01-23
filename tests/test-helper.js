e7dbc3ab92963d338240c95f5cbd5f2465606e75
Edward Faulkner <ef@alum.mit.edu>
2018-01-23 17:46:49 -0500

    fix a new deprecation in ember-qunit

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,7 +1,7 @@
 import resolver from './helpers/resolver';
 import {
   setResolver
-} from 'ember-qunit';
+} from '@ember/test-helpers';
 import { start } from 'ember-cli-qunit';
 
 setResolver(resolver);
