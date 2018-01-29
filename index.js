9e8cbd79f1627ec5fa0bda59d8994f8e37c153c6
Edward Faulkner <ef@alum.mit.edu>
2018-01-29 16:52:53 -0500

    we are supposed to call super here
    without this it's possible to miss recursive included calls in our dependencies

--- index.js
+++ index.js
@@ -107,6 +107,8 @@ module.exports = {
   },
 
   included: function(){
+    this._super.apply(this, arguments);
+
     // We cannot use ember-cli to import velocity as an AMD module here, because we always need the shim in FastBoot
     // to not break any module imports (as velocity/velocity.js has a FastBoot guard, so FastBoot does not see any
     // module inside
