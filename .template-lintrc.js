58905f814e96740e692970beee8343e1cc4c3a7b
Edward Faulkner <ef@alum.mit.edu>
2019-05-23 16:15:32 -0400

    template lints

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended'
+  extends: 'recommended',
+  rules: {
+    "block-indentation": false,
+    "no-outlet-outside-routes": false,
+  }
 };
