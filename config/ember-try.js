8a32f90d497f479b11c1eb767e29d733aa8fa477
Edward Faulkner <ef@alum.mit.edu>
2019-05-24 02:52:00 -0400

    useYarn in ember try

--- config/ember-try.js
+++ config/ember-try.js
@@ -9,6 +9,7 @@ module.exports = function() {
     getChannelURL('canary')
   ]).then((urls) => {
     return {
+      useYarn: true,
       scenarios: [
         {
           name: 'ember-lts-2.12',
