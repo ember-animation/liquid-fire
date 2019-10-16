47502651f06fe4f23969de1e8db831200398ca12
Matt Donnelly <matthew.donnelly@intercom.io>
2019-10-16 14:38:25 +0100

    Add tests

---
+++ tests/integration/helpers/liquid-measured-test.js
@@ -0,0 +1,22 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from "ember-qunit";
+import { render, triggerEvent } from '@ember/test-helpers';
+import sinon from 'sinon';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration: liquid-measured', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it should set correct height when window is resized', async function(assert) {
+    assert.expect(2);
+
+    const didMeasureSpy = sinon.spy();
+    this.set('didMeasureSpy', didMeasureSpy);
+    let template = hbs`{{#liquid-measured didMeasure=didMeasureSpy}}hello{{/liquid-measured}}`
+    await render(template);
+
+    assert.equal(didMeasureSpy.callCount, 1);
+    await triggerEvent(window, 'resize');
+    assert.equal(didMeasureSpy.callCount, 2);
+  });
+});
