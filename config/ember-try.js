254a0a1f6da70e43afe3370362c9a323327c075f
Edward Faulkner <ef@alum.mit.edu>
2021-08-24 11:38:19 -0400

    last-known-good ember-try

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,7 +7,7 @@ module.exports = function() {
     getChannelURL('release'),
     getChannelURL('beta'),
     getChannelURL('canary')
-  ]).then((urls) => {
+  ]).then(() => {
     return {
       useYarn: true,
       scenarios: [
@@ -53,39 +53,47 @@ module.exports = function() {
           }
         },
         {
-          name: 'ember-release',
+          name: 'ember-3.10',
           npm: {
             devDependencies: {
-              'ember-source': urls[0]
+              'ember-source': '^3.10.0'
             }
           }
         },
-        {
-          name: 'ember-beta',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[1]
-            }
-          }
-        },
-        {
-          name: 'ember-canary',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[2]
-            }
-          }
-        },
+        // {
+        //   name: 'ember-release',
+        //   npm: {
+        //     devDependencies: {
+        //       'ember-source': urls[0]
+        //     }
+        //   }
+        // },
+        // {
+        //   name: 'ember-beta',
+        //   npm: {
+        //     devDependencies: {
+        //       'ember-source': urls[1]
+        //     }
+        //   }
+        // },
+        // {
+        //   name: 'ember-canary',
+        //   npm: {
+        //     devDependencies: {
+        //       'ember-source': urls[2]
+        //     }
+        //   }
+        // },
         // The default `.travis.yml` runs this scenario via `npm test`,
         // not via `ember try`. It's still included here so that running
         // `ember try:each` manually or from a customized CI config will run it
         // along with all the other scenarios.
-        {
-          name: 'ember-default',
-          npm: {
-            devDependencies: {}
-          }
-        }
+        // {
+        //   name: 'ember-default',
+        //   npm: {
+        //     devDependencies: {}
+        //   }
+        // }
       ]
     };
   });
