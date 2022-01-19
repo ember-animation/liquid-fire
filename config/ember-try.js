beeee542523184583764a40edea3f5380a00a9eb
Edward Faulkner <edward@eaf4.com>
2022-01-19 16:11:03 -0500

    dropping ember < 3.16

--- config/ember-try.js
+++ config/ember-try.js
@@ -8,23 +8,6 @@ module.exports = async function () {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.8',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.8.0',
-            'ember-angle-bracket-invocation-polyfill': '^3.0.1',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.12',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.12.0',
-          },
-        },
-      },
-      {
         name: 'ember-lts-3.16',
         npm: {
           devDependencies: {
