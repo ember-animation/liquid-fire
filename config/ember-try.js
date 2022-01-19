d9cb44ebebd5423942c5d4725f0ac3510337ce1a
Robert Wagner <rwwagner90@gmail.com>
2022-01-19 14:18:53 -0500

    ember-cli 4.1.0, update embroider

--- config/ember-try.js
+++ config/ember-try.js
@@ -49,6 +49,14 @@ module.exports = async function () {
         },
       },
       {
+        name: 'ember-lts-3.28',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.28.0',
+          },
+        },
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
@@ -82,6 +90,9 @@ module.exports = async function () {
           }),
         },
         npm: {
+          devDependencies: {
+            'ember-source': '~3.28.0',
+          },
           ember: {
             edition: 'classic',
           },
