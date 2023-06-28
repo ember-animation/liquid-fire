4b348ac95d43342030ee2f896570a86686ac735b
Markus Sanin <markus.sanin@zeppelin-group.com>
2023-06-28 08:32:01 +0200

    fix ember-try

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -8,18 +8,27 @@ module.exports = async function () {
     usePnpm: true,
     scenarios: [
       {
-        name: 'ember-lts-3.28',
+        name: 'ember-lts-4.4',
         npm: {
           devDependencies: {
-            'ember-source': '~3.28.0',
+            'ember-resolver': '^8.0.0',
+            'ember-source': '~4.4.0',
           },
         },
       },
       {
-        name: 'ember-lts-4.4',
+        name: 'ember-lts-4.8',
         npm: {
           devDependencies: {
-            'ember-source': '~4.4.0',
+            'ember-source': '~4.8.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.12.0',
           },
         },
       },
@@ -58,10 +67,19 @@ module.exports = async function () {
         },
         npm: {
           devDependencies: {
-            'ember-source': '~3.28.0',
+            'ember-source': '~4.12.0',
           },
           ember: {
             edition: 'classic',
+          },
+        },
+      },
+      {
+        name: 'fastboot',
+        command: 'npm run test:fastboot',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.12.0',
           },
         },
       },
