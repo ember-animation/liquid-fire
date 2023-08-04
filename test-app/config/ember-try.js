fc507780bc454bbd80d7954ce2c7b464900cb6f7
Markus Sanin <markus.sanin@zeppelin-group.com>
2023-08-04 10:45:16 +0200

    Add ember-try for ember 5

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -33,6 +33,14 @@ module.exports = async function () {
         },
       },
       {
+        name: 'ember-5.0',
+        npm: {
+          devDependencies: {
+            'ember-source': '~5.0.0',
+          },
+        },
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
