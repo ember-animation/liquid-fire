16f81f9fcceac7996b2f40fe0779f154256fe4d5
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-08-06 07:47:50 +0200

    Copy capitalize from `@ember/string` package

--- liquid-fire/src/-private/string.js
+++ liquid-fire/src/-private/string.js
@@ -1,3 +1,5 @@
-export function capitalize(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+const CAPITALIZE_REGEX = /(^|\/)([a-z\u00C0-\u024F])/g;
+
+export function capitalize(str) {
+  return str.replace(CAPITALIZE_REGEX, (match) => match.toUpperCase());
 }
